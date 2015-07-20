1865867a6c800142fb547ede2ec01aaa02c8e930
Jan W. Krieger <jan@jkrieger.de>
2015-07-20 19:30:43 +0200

    Update use_tinymatwriter.cpp

--- examples/jank_plugins/use_tinymatwriter.cpp
+++ examples/jank_plugins/use_tinymatwriter.cpp
@@ -1,26 +1,12 @@
-/*
-    Copyright (c) 2008-2015 Jan W. Krieger (<jan@jkrieger.de>, <j.krieger@dkfz.de>), German Cancer Research Center (DKFZ) & IWR, University of Heidelberg
 
-    last modification: $LastChangedDate: 2015-07-07 12:07:58 +0200 (Di, 07 Jul 2015) $  (revision $Rev: 4005 $)
-
-    This software is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License (LGPL) as published by
-    the Free Software Foundation, either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-*/
 
 
 /*
+  Test for the new CImg plugin "plugins/tinymatwriter.h"
+  
+  Copyright:  (c) 2015 by Jan W. Krieger
+  License:    Public Domain (this file!)
+
   This Matlab/Octave script tests the output:
 		clear all
 		more off

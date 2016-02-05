0331a61f7c509f3da1752d057e6c3de39ecc50a7
dtschump <David.Tschumperle@gmail.com>
2016-02-05 14:10:46 +0100

    Final release 1.6.9

--- CImg.h
+++ CImg.h
@@ -9182,8 +9182,10 @@ namespace cimg_library_suffixed {
     typedef typename cimg::last<T,double>::type doubleT;
 #if cimg_OS==2
     typedef typename cimg::last<T,UINT_PTR>::type uptrT;  // Unsigned integer type that can store a pointer.
+    typedef typename cimg::last<T,INT_PTR>::type ptrT; // Signed integer type that can store a pointer.
 #else
     typedef typename cimg::last<T,unsigned long>::type uptrT;
+    typedef typename cimg::last<T,long>::type ptrT;
 #endif
 
     //@}
@@ -39484,10 +39486,10 @@ namespace cimg_library_suffixed {
       const t
         *ptrs = sprite._data -
         (bx?x0:0) -
-        (by?y0*(unsigned long)sprite.width():0) -
-        (bz?z0*(unsigned long)sprite.width()*sprite.height():0) -
-        (bc?c0*(unsigned long)sprite.width()*sprite.height()*sprite.depth():0);
-      const unsigned long
+        (by?y0*(uptrT)sprite.width():0) -
+        (bz?z0*(uptrT)sprite.width()*sprite.height():0) -
+        (bc?c0*(uptrT)sprite.width()*sprite.height()*sprite.depth():0);
+      const uptrT
         offX = (unsigned long)_width - lX,
         soffX = (unsigned long)sprite._width - lX,
         offY = (unsigned long)_width*(_height - lY),
@@ -39528,9 +39530,9 @@ namespace cimg_library_suffixed {
       const T
         *ptrs = sprite._data -
         (bx?x0:0) -
-        (by?y0*(unsigned long)sprite.width():0) -
-        (bz?z0*(unsigned long)sprite.width()*sprite.height():0) -
-        (bc?c0*(unsigned long)sprite.width()*sprite.height()*sprite.depth():0);
+        (by?y0*(uptrT)sprite.width():0) -
+        (bz?z0*(uptrT)sprite.width()*sprite.height():0) -
+        (bc?c0*(uptrT)sprite.width()*sprite.height()*sprite.depth():0);
       const unsigned long
         offX = (unsigned long)_width - lX,
         soffX = (unsigned long)sprite._width - lX,
@@ -39620,12 +39622,12 @@ namespace cimg_library_suffixed {
         lY = sprite.height() - (y0 + sprite.height()>height()?y0 + sprite.height() - height():0) + (by?y0:0),
         lZ = sprite.depth() - (z0 + sprite.depth()>depth()?z0 + sprite.depth() - depth():0) + (bz?z0:0),
         lC = sprite.spectrum() - (c0 + sprite.spectrum()>spectrum()?c0 + sprite.spectrum() - spectrum():0) + (bc?c0:0);
-      const unsigned long
+      const uptrT
         coff = -(bx?x0:0) -
-        (by?y0*(unsigned long)mask.width():0) -
-        (bz?z0*(unsigned long)mask.width()*mask.height():0) -
-        (bc?c0*(unsigned long)mask.width()*mask.height()*mask.depth():0),
-        ssize = (unsigned long)mask.width()*mask.height()*mask.depth()*mask.spectrum();
+        (by?y0*(uptrT)mask.width():0) -
+        (bz?z0*(uptrT)mask.width()*mask.height():0) -
+        (bc?c0*(uptrT)mask.width()*mask.height()*mask.depth():0),
+        ssize = (uptrT)mask.width()*mask.height()*mask.depth()*mask.spectrum();
       const ti *ptrs = sprite._data + coff;
       const tm *ptrm = mask._data + coff;
       const unsigned long

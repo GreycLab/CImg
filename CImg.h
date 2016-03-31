fd34ee919e5e87a07cea4b69027e64358cad8f6a
dtschump <David.Tschumperle@gmail.com>
2016-03-31 13:01:40 +0200

    .

--- CImg.h
+++ CImg.h
@@ -34435,7 +34435,7 @@ namespace cimg_library_suffixed {
 #else
       switch (cimg::uncase(axis)) {
       case 'x' : { // Fourier along X, using built-in functions.
-        const unsigned int N = real._width, N2 = (N>>1);
+        const unsigned int N = real._width, N2 = N>>1;
         if (((N - 1)&N) && N!=1)
           throw CImgInstanceException("CImgList<%s>::FFT(): Specified real and imaginary parts (%u,%u,%u,%u) "
                                       "have non 2^N dimension along the X-axis.",
@@ -34444,21 +34444,24 @@ namespace cimg_library_suffixed {
 
         for (unsigned int i = 0, j = 0; i<N2; ++i) {
           if (j>i) cimg_forYZC(real,y,z,c) {
-            cimg::swap(real(i,y,z,c),real(j,y,z,c)); cimg::swap(imag(i,y,z,c),imag(j,y,z,c));
-            if (j<N2) {
-              const unsigned int ri = N - 1 - i, rj = N - 1 - j;
-              cimg::swap(real(ri,y,z,c),real(rj,y,z,c)); cimg::swap(imag(ri,y,z,c),imag(rj,y,z,c));
+              cimg::swap(real(i,y,z,c),real(j,y,z,c));
+              cimg::swap(imag(i,y,z,c),imag(j,y,z,c));
+              if (j<N2) {
+                const unsigned int ri = N - 1 - i, rj = N - 1 - j;
+                cimg::swap(real(ri,y,z,c),real(rj,y,z,c));
+                cimg::swap(imag(ri,y,z,c),imag(rj,y,z,c));
+              }
             }
-          }
           for (unsigned int m = N, n = N2; (j+=n)>=m; j-=m, m = n, n>>=1) {}
         }
         for (unsigned int delta = 2; delta<=N; delta<<=1) {
-          const unsigned int delta2 = (delta>>1);
+          const unsigned int delta2 = delta>>1;
           for (unsigned int i = 0; i<N; i+=delta) {
             float wr = 1, wi = 0;
-            const float angle = (float)((is_invert?+1:-1)*2*cimg::PI/delta),
-                        ca = (float)std::cos(angle),
-                        sa = (float)std::sin(angle);
+            const float
+              angle = (float)((is_invert?+1:-1)*2*cimg::PI/delta),
+              ca = (float)std::cos(angle),
+              sa = (float)std::sin(angle);
             for (unsigned int k = 0; k<delta2; ++k) {
               const unsigned int j = i + k, nj = j + delta2;
               cimg_forYZC(real,y,z,c) {
@@ -34478,7 +34481,7 @@ namespace cimg_library_suffixed {
         if (is_invert) { real/=N; imag/=N; }
       } break;
       case 'y' : { // Fourier along Y, using built-in functions.
-        const unsigned int N = real._height, N2 = (N>>1);
+        const unsigned int N = real._height, N2 = N>>1;
         if (((N - 1)&N) && N!=1)
           throw CImgInstanceException("CImgList<%s>::FFT(): Specified real and imaginary parts (%u,%u,%u,%u) "
                                       "have non 2^N dimension along the Y-axis.",
@@ -34487,20 +34490,24 @@ namespace cimg_library_suffixed {
 
         for (unsigned int i = 0, j = 0; i<N2; ++i) {
           if (j>i) cimg_forXZC(real,x,z,c) {
-            cimg::swap(real(x,i,z,c),real(x,j,z,c)); cimg::swap(imag(x,i,z,c),imag(x,j,z,c));
-            if (j<N2) {
-              const unsigned int ri = N - 1 - i, rj = N - 1 - j;
-              cimg::swap(real(x,ri,z,c),real(x,rj,z,c)); cimg::swap(imag(x,ri,z,c),imag(x,rj,z,c));
+              cimg::swap(real(x,i,z,c),real(x,j,z,c));
+              cimg::swap(imag(x,i,z,c),imag(x,j,z,c));
+              if (j<N2) {
+                const unsigned int ri = N - 1 - i, rj = N - 1 - j;
+                cimg::swap(real(x,ri,z,c),real(x,rj,z,c));
+                cimg::swap(imag(x,ri,z,c),imag(x,rj,z,c));
+              }
             }
-          }
           for (unsigned int m = N, n = N2; (j+=n)>=m; j-=m, m = n, n>>=1) {}
         }
         for (unsigned int delta = 2; delta<=N; delta<<=1) {
           const unsigned int delta2 = (delta>>1);
           for (unsigned int i = 0; i<N; i+=delta) {
             float wr = 1, wi = 0;
-            const float angle = (float)((is_invert?+1:-1)*2*cimg::PI/delta),
-                        ca = (float)std::cos(angle), sa = (float)std::sin(angle);
+            const float
+              angle = (float)((is_invert?+1:-1)*2*cimg::PI/delta),
+              ca = (float)std::cos(angle),
+              sa = (float)std::sin(angle);
             for (unsigned int k = 0; k<delta2; ++k) {
               const unsigned int j = i + k, nj = j + delta2;
               cimg_forXZC(real,x,z,c) {
@@ -34520,7 +34527,7 @@ namespace cimg_library_suffixed {
         if (is_invert) { real/=N; imag/=N; }
       } break;
       case 'z' : { // Fourier along Z, using built-in functions.
-        const unsigned int N = real._depth, N2 = (N>>1);
+        const unsigned int N = real._depth, N2 = N>>1;
         if (((N - 1)&N) && N!=1)
           throw CImgInstanceException("CImgList<%s>::FFT(): Specified real and imaginary parts (%u,%u,%u,%u) "
                                       "have non 2^N dimension along the Z-axis.",
@@ -34529,20 +34536,24 @@ namespace cimg_library_suffixed {
 
         for (unsigned int i = 0, j = 0; i<N2; ++i) {
           if (j>i) cimg_forXYC(real,x,y,c) {
-            cimg::swap(real(x,y,i,c),real(x,y,j,c)); cimg::swap(imag(x,y,i,c),imag(x,y,j,c));
-            if (j<N2) {
-              const unsigned int ri = N - 1 - i, rj = N - 1 - j;
-              cimg::swap(real(x,y,ri,c),real(x,y,rj,c)); cimg::swap(imag(x,y,ri,c),imag(x,y,rj,c));
+              cimg::swap(real(x,y,i,c),real(x,y,j,c));
+              cimg::swap(imag(x,y,i,c),imag(x,y,j,c));
+              if (j<N2) {
+                const unsigned int ri = N - 1 - i, rj = N - 1 - j;
+                cimg::swap(real(x,y,ri,c),real(x,y,rj,c));
+                cimg::swap(imag(x,y,ri,c),imag(x,y,rj,c));
+              }
             }
-          }
           for (unsigned int m = N, n = N2; (j+=n)>=m; j-=m, m = n, n>>=1) {}
         }
         for (unsigned int delta = 2; delta<=N; delta<<=1) {
           const unsigned int delta2 = (delta>>1);
           for (unsigned int i = 0; i<N; i+=delta) {
             float wr = 1, wi = 0;
-            const float angle = (float)((is_invert?+1:-1)*2*cimg::PI/delta),
-                        ca = (float)std::cos(angle), sa = (float)std::sin(angle);
+            const float
+              angle = (float)((is_invert?+1:-1)*2*cimg::PI/delta),
+              ca = (float)std::cos(angle),
+              sa = (float)std::sin(angle);
             for (unsigned int k = 0; k<delta2; ++k) {
               const unsigned int j = i + k, nj = j + delta2;
               cimg_forXYC(real,x,y,c) {

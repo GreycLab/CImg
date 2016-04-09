68a55cc303302ccea59d332f225b2ae228801f86
David Tschumperlé <David.Tschumperle@gmail.com>
2016-04-09 14:30:19 +0200

    .

--- CImg.h
+++ CImg.h
@@ -6659,11 +6659,14 @@ namespace cimg_library_suffixed {
         if (img._depth==1 && (img._spectrum==1 || img._spectrum>=3) && _normalization!=1) return display(img);
       }
       CImgList<typename CImg<T>::ucharT> visu(list._width);
+      unsigned int dims = 0;
       cimglist_for(list,l) {
         const CImg<T>& img = list._data[l];
         img.__get_select(*this,_normalization,(img._width - 1)/2,(img._height - 1)/2,
                          (img._depth - 1)/2).move_to(visu[l]);
+        dims = cimg::max(dims,visu[l]._spectrum);
       }
+      cimglist_for(list,l) if (visu[l]._spectrum<dims) visu[l].resize(-100,-100,-100,dims,1);
       visu.get_append(axis,align).display(*this);
       return *this;
     }

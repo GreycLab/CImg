a3ab40b14788d79ae132da8833d3d279bcda222f
David Tschumperlé <David.Tschumperle@gmail.com>
2016-05-25 08:47:13 +0200

    .

--- CImg.h
+++ CImg.h
@@ -54510,14 +54510,12 @@ namespace cimg_library_suffixed {
         }
         res.append(str,'x');
       }
-      if (!res) CImg<charT>(1,1,1,1,0).move_to(res);
-      else {
-        cimg::strellipsize(res,128,false);
-        if (_width>1) {
-          const unsigned int l = std::strlen(res);
-          if (res._width<=l + 16) res.resize(l + 16,1,1,1,0);
-          cimg_snprintf(res._data + l,16," [%u]",_width);
-        }
+      if (!res) return CImg<charT>(1,1,1,1,0).move_to(res);
+      cimg::strellipsize(res,128,false);
+      if (_width>1) {
+        const unsigned int l = std::strlen(res);
+        if (res._width<=l + 16) res.resize(l + 16,1,1,1,0);
+        cimg_snprintf(res._data + l,16," [%u]",_width);
       }
       return res;
     }

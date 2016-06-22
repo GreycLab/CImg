daaa638af42e5ff690856234988afa7fcbcc2996
David Tschumperlé <David.Tschumperle@gmail.com>
2016-06-22 10:19:52 +0200

    .

--- CImg.h
+++ CImg.h
@@ -55752,21 +55752,21 @@ namespace cimg {
 #if cimg_OS==2
     // Convert 'path' to a wide-character string.
     int err = MultiByteToWideChar(CP_UTF8,0,path,-1,0,0);
-    if (!err) return std::fopen(path,mode);
+    if (!err) return std_fopen(path,mode);
     CImg<wchar_t> wpath(err);
     err = MultiByteToWideChar(CP_UTF8,0,path,-1,wpath,err);
-    if (!err) return std::fopen(path,mode);
+    if (!err) return std_fopen(path,mode);
 
     // Convert 'mode' to a wide-character string.
     err = MultiByteToWideChar(CP_UTF8,0,mode,-1,0,0);
-    if (!err) return std::fopen(path,mode);
+    if (!err) return std_fopen(path,mode);
     CImg<wchar_t> wmode(err);
     err = MultiByteToWideChar(CP_UTF8,0,mode,-1,wmode,err);
-    if (!err) return std::fopen(path,mode);
+    if (!err) return std_fopen(path,mode);
 
     return _wfopen(wpath,wmode);
 #else
-    return std::fopen(path,mode);
+    return std_fopen(path,mode);
 #endif
   }
 

b03b589191a47c147a2f2d1a980e3965738a3cad
David Tschumperlé <David.Tschumperle@gmail.com>
2016-07-08 20:43:56 +0200

    .

--- CImg.h
+++ CImg.h
@@ -19498,7 +19498,7 @@ namespace cimg_library_suffixed {
       static double mp_rot2d(_cimg_math_parser& mp) {
         double *ptrd = &_mp_arg(1) + 1;
         const float
-          theta = (float)_mp_arg(2),
+          theta = (float)_mp_arg(2)*cimg::PI/180,
           ca = std::cos(theta),
           sa = std::sin(theta);
         *(ptrd++) = ca;

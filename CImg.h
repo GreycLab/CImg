e0efa8e791dedfff20d3be7dc07e6db04937b0d2
David Tschumperlé <David.Tschumperle@gmail.com>
2016-07-10 08:44:07 +0200

    .

--- CImg.h
+++ CImg.h
@@ -4730,6 +4730,11 @@ namespace cimg_library_suffixed {
       return std::log(x)/base;
     }
 
+    template<typename T>
+    inline T round(const T& x) {
+      return (T)std::floor((_cimg_Tfloat)x + 0.5f);
+    }
+
     //! Return rounded value.
     /**
        \param x Value to be rounded.
@@ -4740,13 +4745,13 @@ namespace cimg_library_suffixed {
     template<typename T>
     inline T round(const T& x, const double y, const int rounding_type=0) {
       if (y<=0) return x;
+      if (y==1) switch (rounding_type) {
+        case 0 : return round(x);
+        case 1 : return (T)std::ceil((_cimg_Tfloat)x);
+        default : return (T)std::floor((_cimg_Tfloat)x);
+        }
       const double sx = (double)x/y, floor = std::floor(sx), delta =  sx - floor;
       return (T)(y*(rounding_type<0?floor:rounding_type>0?std::ceil(sx):delta<0.5?floor:std::ceil(sx)));
-    }
-
-    template<typename T>
-    inline T round(const T& x) {
-      return (T)(int)(x + (x>=0?0.5f:-0.5f));
     }
 
     inline double _pythagore(double a, double b) {

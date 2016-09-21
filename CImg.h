02ec481605f4dd68dec9d8c5b7b4bd763ad31cac
dtschump <David.Tschumperle@gmail.com>
2016-09-21 13:21:28 +0200

    .

--- CImg.h
+++ CImg.h
@@ -25947,6 +25947,165 @@ namespace cimg_library_suffixed {
       return CImg<Tfloat>(*this,false).RGBtosRGB();
     }
 
+    //! Convert pixel values from RGB to HSI color spaces.
+    CImg<T>& RGBtoHSI() {
+      if (_spectrum!=3)
+        throw CImgInstanceException(_cimg_instance
+                                    "RGBtoHSI(): Instance is not a RGB image.",
+                                    cimg_instance);
+
+      T *p1 = data(0,0,0,0), *p2 = data(0,0,0,1), *p3 = data(0,0,0,2);
+      const ulongT whd = (ulongT)_width*_height*_depth;
+      cimg_pragma_openmp(parallel for cimg_openmp_if(whd>=256))
+      for (ulongT N = 0; N<whd; ++N) {
+        const Tfloat
+          R = (Tfloat)p1[N],
+          G = (Tfloat)p2[N],
+          B = (Tfloat)p3[N],
+          theta = (Tfloat)(std::acos(0.5f*((R - G) + (R - B))/
+                                     std::sqrt(cimg::sqr(R - G) + (R - B)*(G - B)))*180/cimg::PI),
+          m = cimg::min(R,G,B),
+          sum = R + G + B;
+        Tfloat H = 0, S = 0, I = 0;
+        if (theta>0) H = B<=G?theta:360 - theta;
+        if (sum>0) S = 1 - 3*m/sum;
+        I = sum/(3*255);
+        p1[N] = (T)cimg::cut(H,0,360);
+        p2[N] = (T)cimg::cut(S,0,1);
+        p3[N] = (T)cimg::cut(I,0,1);
+      }
+      return *this;
+    }
+
+    //! Convert pixel values from RGB to HSI color spaces \newinstance.
+    CImg<Tfloat> get_RGBtoHSI() const {
+      return CImg<Tfloat>(*this,false).RGBtoHSI();
+    }
+
+    //! Convert pixel values from HSI to RGB color spaces.
+    CImg<T>& HSItoRGB() {
+      if (_spectrum!=3)
+        throw CImgInstanceException(_cimg_instance
+                                    "HSItoRGB(): Instance is not a HSI image.",
+                                    cimg_instance);
+
+      T *p1 = data(0,0,0,0), *p2 = data(0,0,0,1), *p3 = data(0,0,0,2);
+      const ulongT whd = (ulongT)_width*_height*_depth;
+      cimg_pragma_openmp(parallel for cimg_openmp_if(whd>=256))
+      for (ulongT N = 0; N<whd; ++N) {
+        Tfloat
+          H = cimg::mod((Tfloat)p1[N],(Tfloat)360),
+          S = (Tfloat)p2[N],
+          I = (Tfloat)p3[N],
+          a = I*(1 - S),
+          R = 0, G = 0, B = 0;
+        if (H<120) {
+          B = a;
+          R = (Tfloat)(I*(1 + S*std::cos(H*cimg::PI/180)/std::cos((60 - H)*cimg::PI/180)));
+          G = 3*I - (R + B);
+        } else if (H<240) {
+          H-=120;
+          R = a;
+          G = (Tfloat)(I*(1 + S*std::cos(H*cimg::PI/180)/std::cos((60 - H)*cimg::PI/180)));
+          B = 3*I - (R + G);
+        } else {
+          H-=240;
+          G = a;
+          B = (Tfloat)(I*(1 + S*std::cos(H*cimg::PI/180)/std::cos((60 - H)*cimg::PI/180)));
+          R = 3*I - (G + B);
+        }
+        p1[N] = (T)cimg::cut(R*255,0,255);
+        p2[N] = (T)cimg::cut(G*255,0,255);
+        p3[N] = (T)cimg::cut(B*255,0,255);
+      }
+      return *this;
+    }
+
+    //! Convert pixel values from HSI to RGB color spaces \newinstance.
+    CImg<Tfloat> get_HSItoRGB() const {
+      return CImg< Tuchar>(*this,false).HSItoRGB();
+    }
+
+    //! Convert pixel values from RGB to HSL color spaces.
+    CImg<T>& RGBtoHSL() {
+      if (_spectrum!=3)
+        throw CImgInstanceException(_cimg_instance
+                                    "RGBtoHSL(): Instance is not a RGB image.",
+                                    cimg_instance);
+
+      T *p1 = data(0,0,0,0), *p2 = data(0,0,0,1), *p3 = data(0,0,0,2);
+      const ulongT whd = (ulongT)_width*_height*_depth;
+      cimg_pragma_openmp(parallel for cimg_openmp_if(whd>=256))
+      for (ulongT N = 0; N<whd; ++N) {
+        const Tfloat
+          R = (Tfloat)p1[N],
+          G = (Tfloat)p2[N],
+          B = (Tfloat)p3[N],
+          m = cimg::min(R,G,B),
+          M = cimg::max(R,G,B),
+          L = (m + M)/(2*255);
+        Tfloat H = 0, S = 0;
+        if (M==m) H = S = 0;
+        else {
+          const Tfloat
+            f = R==m?G - B:G==m?B - R:R - G,
+            i = R==m?3:G==m?5:1;
+          H = i - f/(M - m);
+          if (H>=6) H-=6;
+          H*=60;
+          S = 2*L<=1?(M - m)/(M + m):(M - m)/(2*255 - M - m);
+        }
+        p1[N] = (T)cimg::cut(H,0,360);
+        p2[N] = (T)cimg::cut(S,0,1);
+        p3[N] = (T)cimg::cut(L,0,1);
+      }
+      return *this;
+    }
+
+    //! Convert pixel values from RGB to HSL color spaces \newinstance.
+    CImg<Tfloat> get_RGBtoHSL() const {
+      return CImg<Tfloat>(*this,false).RGBtoHSL();
+    }
+
+    //! Convert pixel values from HSL to RGB color spaces.
+    CImg<T>& HSLtoRGB() {
+      if (_spectrum!=3)
+        throw CImgInstanceException(_cimg_instance
+                                    "HSLtoRGB(): Instance is not a HSL image.",
+                                    cimg_instance);
+
+      T *p1 = data(0,0,0,0), *p2 = data(0,0,0,1), *p3 = data(0,0,0,2);
+      const ulongT whd = (ulongT)_width*_height*_depth;
+      cimg_pragma_openmp(parallel for cimg_openmp_if(whd>=256))
+      for (ulongT N = 0; N<whd; ++N) {
+        const Tfloat
+          H = cimg::mod((Tfloat)p1[N],(Tfloat)360),
+          S = (Tfloat)p2[N],
+          L = (Tfloat)p3[N],
+          q = 2*L<1?L*(1 + S):L + S - L*S,
+          p = 2*L - q,
+          h = H/360,
+          tr = h + (Tfloat)1/3,
+          tg = h,
+          tb = h - (Tfloat)1/3,
+          ntr = tr<0?tr + 1:tr>1?tr - 1:tr,
+          ntg = tg<0?tg + 1:tg>1?tg - 1:tg,
+          ntb = tb<0?tb + 1:tb>1?tb - 1:tb,
+          R = 6*ntr<1?p + (q - p)*6*ntr:2*ntr<1?q:3*ntr<2?p + (q - p)*6*(2.0f/3 - ntr):p,
+          G = 6*ntg<1?p + (q - p)*6*ntg:2*ntg<1?q:3*ntg<2?p + (q - p)*6*(2.0f/3 - ntg):p,
+          B = 6*ntb<1?p + (q - p)*6*ntb:2*ntb<1?q:3*ntb<2?p + (q - p)*6*(2.0f/3 - ntb):p;
+        p1[N] = (T)cimg::cut(255*R,0,255);
+        p2[N] = (T)cimg::cut(255*G,0,255);
+        p3[N] = (T)cimg::cut(255*B,0,255);
+      }
+      return *this;
+    }
+
+    //! Convert pixel values from HSL to RGB color spaces \newinstance.
+    CImg<Tuchar> get_HSLtoRGB() const {
+      return CImg<Tuchar>(*this,false).HSLtoRGB();
+    }
+
     //! Convert pixel values from RGB to HSV color spaces.
     CImg<T>& RGBtoHSV() {
       if (_spectrum!=3)
@@ -26030,165 +26189,6 @@ namespace cimg_library_suffixed {
     //! Convert pixel values from HSV to RGB color spaces \newinstance.
     CImg<Tuchar> get_HSVtoRGB() const {
       return CImg<Tuchar>(*this,false).HSVtoRGB();
-    }
-
-    //! Convert pixel values from RGB to HSL color spaces.
-    CImg<T>& RGBtoHSL() {
-      if (_spectrum!=3)
-        throw CImgInstanceException(_cimg_instance
-                                    "RGBtoHSL(): Instance is not a RGB image.",
-                                    cimg_instance);
-
-      T *p1 = data(0,0,0,0), *p2 = data(0,0,0,1), *p3 = data(0,0,0,2);
-      const ulongT whd = (ulongT)_width*_height*_depth;
-      cimg_pragma_openmp(parallel for cimg_openmp_if(whd>=256))
-      for (ulongT N = 0; N<whd; ++N) {
-        const Tfloat
-          R = (Tfloat)p1[N],
-          G = (Tfloat)p2[N],
-          B = (Tfloat)p3[N],
-          m = cimg::min(R,G,B),
-          M = cimg::max(R,G,B),
-          L = (m + M)/(2*255);
-        Tfloat H = 0, S = 0;
-        if (M==m) H = S = 0;
-        else {
-          const Tfloat
-            f = R==m?G - B:G==m?B - R:R - G,
-            i = R==m?3:G==m?5:1;
-          H = i - f/(M - m);
-          if (H>=6) H-=6;
-          H*=60;
-          S = 2*L<=1?(M - m)/(M + m):(M - m)/(2*255 - M - m);
-        }
-        p1[N] = (T)cimg::cut(H,0,360);
-        p2[N] = (T)cimg::cut(S,0,1);
-        p3[N] = (T)cimg::cut(L,0,1);
-      }
-      return *this;
-    }
-
-    //! Convert pixel values from RGB to HSL color spaces \newinstance.
-    CImg<Tfloat> get_RGBtoHSL() const {
-      return CImg<Tfloat>(*this,false).RGBtoHSL();
-    }
-
-    //! Convert pixel values from HSL to RGB color spaces.
-    CImg<T>& HSLtoRGB() {
-      if (_spectrum!=3)
-        throw CImgInstanceException(_cimg_instance
-                                    "HSLtoRGB(): Instance is not a HSL image.",
-                                    cimg_instance);
-
-      T *p1 = data(0,0,0,0), *p2 = data(0,0,0,1), *p3 = data(0,0,0,2);
-      const ulongT whd = (ulongT)_width*_height*_depth;
-      cimg_pragma_openmp(parallel for cimg_openmp_if(whd>=256))
-      for (ulongT N = 0; N<whd; ++N) {
-        const Tfloat
-          H = cimg::mod((Tfloat)p1[N],(Tfloat)360),
-          S = (Tfloat)p2[N],
-          L = (Tfloat)p3[N],
-          q = 2*L<1?L*(1 + S):L + S - L*S,
-          p = 2*L - q,
-          h = H/360,
-          tr = h + (Tfloat)1/3,
-          tg = h,
-          tb = h - (Tfloat)1/3,
-          ntr = tr<0?tr + 1:tr>1?tr - 1:tr,
-          ntg = tg<0?tg + 1:tg>1?tg - 1:tg,
-          ntb = tb<0?tb + 1:tb>1?tb - 1:tb,
-          R = 6*ntr<1?p + (q - p)*6*ntr:2*ntr<1?q:3*ntr<2?p + (q - p)*6*(2.0f/3 - ntr):p,
-          G = 6*ntg<1?p + (q - p)*6*ntg:2*ntg<1?q:3*ntg<2?p + (q - p)*6*(2.0f/3 - ntg):p,
-          B = 6*ntb<1?p + (q - p)*6*ntb:2*ntb<1?q:3*ntb<2?p + (q - p)*6*(2.0f/3 - ntb):p;
-        p1[N] = (T)cimg::cut(255*R,0,255);
-        p2[N] = (T)cimg::cut(255*G,0,255);
-        p3[N] = (T)cimg::cut(255*B,0,255);
-      }
-      return *this;
-    }
-
-    //! Convert pixel values from HSL to RGB color spaces \newinstance.
-    CImg<Tuchar> get_HSLtoRGB() const {
-      return CImg<Tuchar>(*this,false).HSLtoRGB();
-    }
-
-    //! Convert pixel values from RGB to HSI color spaces.
-    CImg<T>& RGBtoHSI() {
-      if (_spectrum!=3)
-        throw CImgInstanceException(_cimg_instance
-                                    "RGBtoHSI(): Instance is not a RGB image.",
-                                    cimg_instance);
-
-      T *p1 = data(0,0,0,0), *p2 = data(0,0,0,1), *p3 = data(0,0,0,2);
-      const ulongT whd = (ulongT)_width*_height*_depth;
-      cimg_pragma_openmp(parallel for cimg_openmp_if(whd>=256))
-      for (ulongT N = 0; N<whd; ++N) {
-        const Tfloat
-          R = (Tfloat)p1[N],
-          G = (Tfloat)p2[N],
-          B = (Tfloat)p3[N],
-          theta = (Tfloat)(std::acos(0.5f*((R - G) + (R - B))/
-                                     std::sqrt(cimg::sqr(R - G) + (R - B)*(G - B)))*180/cimg::PI),
-          m = cimg::min(R,G,B),
-          sum = R + G + B;
-        Tfloat H = 0, S = 0, I = 0;
-        if (theta>0) H = B<=G?theta:360 - theta;
-        if (sum>0) S = 1 - 3*m/sum;
-        I = sum/(3*255);
-        p1[N] = (T)cimg::cut(H,0,360);
-        p2[N] = (T)cimg::cut(S,0,1);
-        p3[N] = (T)cimg::cut(I,0,1);
-      }
-      return *this;
-    }
-
-    //! Convert pixel values from RGB to HSI color spaces \newinstance.
-    CImg<Tfloat> get_RGBtoHSI() const {
-      return CImg<Tfloat>(*this,false).RGBtoHSI();
-    }
-
-    //! Convert pixel values from HSI to RGB color spaces.
-    CImg<T>& HSItoRGB() {
-      if (_spectrum!=3)
-        throw CImgInstanceException(_cimg_instance
-                                    "HSItoRGB(): Instance is not a HSI image.",
-                                    cimg_instance);
-
-      T *p1 = data(0,0,0,0), *p2 = data(0,0,0,1), *p3 = data(0,0,0,2);
-      const ulongT whd = (ulongT)_width*_height*_depth;
-      cimg_pragma_openmp(parallel for cimg_openmp_if(whd>=256))
-      for (ulongT N = 0; N<whd; ++N) {
-        Tfloat
-          H = cimg::mod((Tfloat)p1[N],(Tfloat)360),
-          S = (Tfloat)p2[N],
-          I = (Tfloat)p3[N],
-          a = I*(1 - S),
-          R = 0, G = 0, B = 0;
-        if (H<120) {
-          B = a;
-          R = (Tfloat)(I*(1 + S*std::cos(H*cimg::PI/180)/std::cos((60 - H)*cimg::PI/180)));
-          G = 3*I - (R + B);
-        } else if (H<240) {
-          H-=120;
-          R = a;
-          G = (Tfloat)(I*(1 + S*std::cos(H*cimg::PI/180)/std::cos((60 - H)*cimg::PI/180)));
-          B = 3*I - (R + G);
-        } else {
-          H-=240;
-          G = a;
-          B = (Tfloat)(I*(1 + S*std::cos(H*cimg::PI/180)/std::cos((60 - H)*cimg::PI/180)));
-          R = 3*I - (G + B);
-        }
-        p1[N] = (T)cimg::cut(R*255,0,255);
-        p2[N] = (T)cimg::cut(G*255,0,255);
-        p3[N] = (T)cimg::cut(B*255,0,255);
-      }
-      return *this;
-    }
-
-    //! Convert pixel values from HSI to RGB color spaces \newinstance.
-    CImg<Tfloat> get_HSItoRGB() const {
-      return CImg< Tuchar>(*this,false).HSItoRGB();
     }
 
     //! Convert pixel values from RGB to YCbCr color spaces.

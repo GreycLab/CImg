d54b141f31afb29ded99a7b7771dcf11bde2749e
David Tschumperlé <David.Tschumperle@gmail.com>
2016-09-25 10:08:01 +0200

    Improve precision of cimg::fibonacci() for high numbers. Contribution by Ingo.

--- CImg.h
+++ CImg.h
@@ -5242,13 +5242,37 @@ namespace cimg_library_suffixed {
       return with_order?res:res/cimg::factorial(k);
     }
 
-    //! Return fibonacci number.
+    inline double _fibonacci(int exp) {
+      double
+        base = 1.6180339887498948482045868343656, // (1 + sqrt(5))/2;
+        result = 0.44721359549995793928183473374626; // 1 / sqrt(5)
+      while (exp) {
+        if (exp&1) result*=base;
+        exp>>=1;
+        base*=base;
+      }
+      return result;
+    }
+
+    //! Calculate fibonacci number.
+    // (Precise up to n = 78, less precise for n>78).
     inline double fibonacci(const int n) {
       if (n<0) return cimg::type<double>::nan();
       if (n<3) return 1;
-      cimg_long fn1 = 1, fn2 = 1, fn = 2;
-      for (int i = 3; i<=n; ++i) { fn = fn1 + fn2; fn2 = fn1; fn1 = fn; }
-      return (double)fn;
+      if (n<11) {
+          cimg_uint64 fn1 = 1, fn2 = 1, fn;
+          for (int i = 3; i<=n; ++i) { fn = fn1 + fn2; fn2 = fn1; fn1 = fn; }
+          return (double)fn;
+      }
+      if(n<75) // precise up to n = 74, faster than the integer calculation above for n>10
+        return (double)((cimg_uint64)(_fibonacci(n) + 0.5));
+
+      if(n<94) { // precise up to n = 78, less precise for n>78 up to n = 93, overflows for n>93
+          cimg_uint64 fn1 = 1304969544928657, fn2 = 806515533049393, fn;
+          for (int i = 75; i<=n; ++i) { fn = fn1 + fn2; fn2 = fn1; fn1 = fn; }
+          return (double)fn;
+      }
+      return _fibonacci(n); // Not precise, but better than the wrong overflowing calculation
     }
 
     //! Convert ascii character to lower case.

f22f4fb9da88e721fdd1bbacbfa02f3df9974145
dtschump <David.Tschumperle@gmail.com>
2016-09-29 17:00:56 +0200

    .

--- CImg.h
+++ CImg.h
@@ -5268,7 +5268,7 @@ namespace cimg_library_suffixed {
         return (double)((cimg_uint64)(_fibonacci(n) + 0.5));
 
       if (n<94) { // precise up to n = 78, less precise for n>78 up to n = 93, overflows for n>93
-        cimg_uint64 fn1 = 1304969544928657, fn2 = 806515533049393, fn;
+        cimg_uint64 fn1 = 1304969544928657, fn2 = 806515533049393, fn = 0;
         for (int i = 75; i<=n; ++i) { fn = fn1 + fn2; fn2 = fn1; fn1 = fn; }
         return (double)fn;
       }
@@ -18732,7 +18732,7 @@ namespace cimg_library_suffixed {
       static double mp_complex_log(_cimg_math_parser& mp) {
         double *ptrd = &_mp_arg(1) + 1;
         const double *ptrs = &_mp_arg(2) + 1, r = *(ptrs++), i = *(ptrs);
-        *(ptrd++) = std::log(cimg::_hypot(r,i));
+        *(ptrd++) = 0.5*std::log(r*r + i*i);
         *(ptrd++) = std::atan2(i,r);
         return cimg::type<double>::nan();
       }
@@ -20660,7 +20660,7 @@ namespace cimg_library_suffixed {
         switch (mp.opcode[2] - 4) {
         case 0 : std::memset(mp.mem._data + ptrd,0,siz*sizeof(double)); break; // 0 values given
         case 1 : { const double val = _mp_arg(ptrs); while (siz-->0) mp.mem[ptrd++] = val; } break;
-        default : while (siz-->0) { mp.mem[ptrd++] = _mp_arg(ptrs++); if (ptrs>=mp.opcode[3]) ptrs = 4U; }
+        default : while (siz-->0) { mp.mem[ptrd++] = _mp_arg(ptrs++); if (ptrs>=mp.opcode[2]) ptrs = 4U; }
         }
         return cimg::type<double>::nan();
       }

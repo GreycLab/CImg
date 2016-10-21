e95bdef76ecaa5936c6f733e98b36f2c65a4cf1f
dtschump <David.Tschumperle@gmail.com>
2016-10-21 15:41:34 +0200

    .

--- CImg.h
+++ CImg.h
@@ -14567,18 +14567,18 @@ namespace cimg_library_suffixed {
         unsigned int mode = 0, next_mode = 0; // { 0=normal | 1=char-string | 2=vector-string
         CImg<uintT> res(expr._width - 1);
         unsigned int *pd = res._data;
-        int lv = 0;
-        for (const char *ps = expr._data; *ps && lv>=0; ++ps) {
+        int level = 0;
+        for (const char *ps = expr._data; *ps && level>=0; ++ps) {
           if (!next_is_escaped && *ps=='\\') next_is_escaped = true;
           if (!is_escaped && *ps=='\'') { // Non-escaped character
             if (!mode && ps>expr._data && *(ps - 1)=='[') next_mode = mode = 2; // Start vector-string
             else if (mode==2 && *(ps + 1)==']') next_mode = !mode; // End vector-string
             else if (mode<2) next_mode = mode?(mode = 0):1; // Start/end char-string
           }
-          *(pd++) = (unsigned int)(mode>=1 || is_escaped?lv + (mode==1):
-                                   *ps=='(' || *ps=='['?lv++:
-                                   *ps==')' || *ps==']'?--lv:
-                                   lv);
+          *(pd++) = (unsigned int)(mode>=1 || is_escaped?level + (mode==1):
+                                   *ps=='(' || *ps=='['?level++:
+                                   *ps==')' || *ps==']'?--level:
+                                   level);
           mode = next_mode;
           is_escaped = next_is_escaped;
           next_is_escaped = false;
@@ -14590,7 +14590,7 @@ namespace cimg_library_suffixed {
                                       pixel_type(),_cimg_mp_calling_function,
                                       expr._data);
         }
-        if (lv!=0) {
+        if (level) {
           cimg::strellipsize(expr,64);
           throw CImgArgumentException("[_cimg_math_parser] "
                                       "CImg<%s>::%s: Unbalanced parentheses/brackets, in expression '%s'.",

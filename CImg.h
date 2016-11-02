1fc3875942f17f640a3aa994db38daaf4483c0e5
dtschump <David.Tschumperle@gmail.com>
2016-11-02 14:03:11 +0100

    Auto-commit for release 1.7.9_pre

--- CImg.h
+++ CImg.h
@@ -7838,7 +7838,7 @@ namespace cimg_library_suffixed {
     **/
     template<typename T>
     static void screenshot(const int x0, const int y0, const int x1, const int y1, CImg<T>& img) {
-      cimg::unused(x0,y0,x1,y1,img);
+      cimg::unused(x0,y0,x1,y1,&img);
       _no_display_exception();
     }
 

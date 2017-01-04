23995209ffb02151a90e0eb974155b09168746cd
dtschump <David.Tschumperle@gmail.com>
2017-01-04 11:29:45 +0100

    Optimize stack usage in method 'CImg<T>::draw_fill()'.

--- CImg.h
+++ CImg.h
@@ -43370,6 +43370,15 @@ namespace cimg_library_suffixed {
       return *this;
     }
 
+    bool _draw_fill(const int x, const int y, const int z,
+                     const CImg<T>& ref, const float tolerance2) const {
+      const T *ptr1 = data(x,y,z), *ptr2 = ref._data;
+      const unsigned long off = _width*_height*_depth;
+      float diff = 0;
+      cimg_forC(*this,c) { diff += cimg::sqr(*ptr1 - *(ptr2++)); ptr1+=off; }
+      return diff<=tolerance2;
+    }
+
     //! Draw filled 3d region with the flood fill algorithm.
     /**
        \param x0 X-coordinate of the starting point of the region to fill.
@@ -43382,24 +43391,12 @@ namespace cimg_library_suffixed {
        \param is_high_connexity Tells if 8-connexity must be used.
        \return \c region is initialized with the binary mask of the filled region.
     **/
-
-
-
-    bool _draw_fill(const int x, const int y, const int z,
-                    const CImg<T>& ref, const float tolerance2) const {
-      const T *ptr1 = data(x,y,z), *ptr2 = ref._data;
-      const unsigned long off = _width*_height*_depth;
-      float diff = 0;
-      cimg_forC(*this,c) { diff += cimg::sqr(*ptr1 - *(ptr2++)); ptr1+=off; }
-      return diff<=tolerance2;
-    }
-
     template<typename tc, typename t>
     CImg<T>& draw_fill(const int x0, const int y0, const int z0,
-                       const tc *const color, const float opacity,
-                       CImg<t> &region,
-                       const float tolerance = 0,
-                       const bool is_high_connectivity = false) {
+                        const tc *const color, const float opacity,
+                        CImg<t> &region,
+                        const float tolerance = 0,
+                        const bool is_high_connectivity = false) {
 #define _draw_fill_push(x,y,z) if (N>=stack._width) stack.resize(2*N + 1,1,1,3,0); \
                                stack[N] = x; stack(N,1) = y; stack(N++,2) = z
 #define _draw_fill_pop(x,y,z) x = stack[--N]; y = stack(N,1); z = stack(N,2)
@@ -43409,7 +43406,7 @@ namespace cimg_library_suffixed {
       const float nopacity = cimg::abs((float)opacity), copacity = 1 - std::max((float)opacity,0.0f);
       const float tolerance2 = cimg::sqr(tolerance);
       const CImg<T> ref = get_vector_at(x0,y0,z0);
-      CImg<uintT> stack(_width,1,1,3);
+      CImg<uintT> stack(_height,1,1,3);
       CImg<ucharT> _region(_width,_height,_depth,1,0);
       unsigned int N = 0;
       int x, y, z;
@@ -43421,48 +43418,64 @@ namespace cimg_library_suffixed {
           const int yp = y - 1, yn = y + 1, zp = z - 1, zn = z + 1;
           int xl = x, xr = x;
 
+          // Using these booleans reduces the number of pushes drastically.
+          bool is_yp = false, is_yn = false, is_zp = false, is_zn = false;
+
           for (int step = -1; step<2; step+=2) {
             while (x>=0 && x<width() && _draw_fill_is_inside(x,y,z)) {
-              if (yp>=0 && _draw_fill_is_inside(x,yp,z)) { _draw_fill_push(x,yp,z); }
-              if (yn<height() && _draw_fill_is_inside(x,yn,z)) { _draw_fill_push(x,yn,z); }
+              if (yp>=0 && _draw_fill_is_inside(x,yp,z)) {
+                if (!is_yp) { _draw_fill_push(x,yp,z); is_yp = true; }
+              } else is_yp = false;
+              if (yn<height() && _draw_fill_is_inside(x,yn,z)) {
+                if (!is_yn) { _draw_fill_push(x,yn,z); is_yn = true; }
+              } else is_yn = false;
+
               if (depth()>1) {
-                if (zp>=0 && _draw_fill_is_inside(x,y,zp)) { _draw_fill_push(x,y,zp); }
-                if (zn<depth() && _draw_fill_is_inside(x,y,zn)) { _draw_fill_push(x,y,zn); }
+                if (zp>=0 && _draw_fill_is_inside(x,y,zp)) {
+                  if (!is_zp) { _draw_fill_push(x,y,zp); is_zp = true; }
+                } else is_zp = false;
+                if (zn<depth() && _draw_fill_is_inside(x,y,zn)) {
+                  if (!is_zn) { _draw_fill_push(x,y,zn); is_zn = true; }
+                } else is_zn = false;
               }
               if (is_high_connectivity) {
                 const int xp = x - 1, xn = x + 1;
-                if (yp>=0) {
+                if (yp>=0 && !is_yp) {
                   if (xp>=0 && _draw_fill_is_inside(xp,yp,z)) { _draw_fill_push(xp,yp,z); }
                   if (xn<width() && _draw_fill_is_inside(xn,yp,z)) { _draw_fill_push(xn,yp,z); }
                 }
-                if (yn<height()) {
+                if (yn<height() && !is_yn) {
                   if (xp>=0 && _draw_fill_is_inside(xp,yn,z)) { _draw_fill_push(xp,yn,z); }
                   if (xn<width() && _draw_fill_is_inside(xn,yn,z)) { _draw_fill_push(xn,yn,z); }
                 }
                 if (depth()>1) {
-                  if (zp>=0) {
-                    if (yp>=0) {
+                  if (zp>=0 && !is_zp) {
+                    if (yp>=0 && !is_yp) {
                       if (xp>=0 && _draw_fill_is_inside(xp,yp,zp)) { _draw_fill_push(xp,yp,zp); }
                       if (_draw_fill_is_inside(x,yp,zp)) { _draw_fill_push(x,yp,zp); }
                       if (xn<width() && _draw_fill_is_inside(xn,yp,zp)) { _draw_fill_push(xn,yp,zp); }
                     }
+
                     if (xp>=0 && _draw_fill_is_inside(xp,y,zp)) { _draw_fill_push(xp,y,zp); }
                     if (xn<width() && _draw_fill_is_inside(xn,y,zp)) { _draw_fill_push(xn,y,zp); }
-                    if (yn<height()) {
+
+                    if (yn<height() && !is_yn) {
                       if (xp>=0 && _draw_fill_is_inside(xp,yn,zp)) { _draw_fill_push(xp,yn,zp); }
                       if (_draw_fill_is_inside(x,yn,zp)) { _draw_fill_push(x,yn,zp); }
                       if (xn<width() && _draw_fill_is_inside(xn,yn,zp)) { _draw_fill_push(xn,yn,zp); }
                     }
                   }
-                  if (zn<depth()) {
-                    if (yp>=0) {
+                  if (zn<depth() && !is_zn) {
+                    if (yp>=0 && !is_yp) {
                       if (xp>=0 && _draw_fill_is_inside(xp,yp,zn)) { _draw_fill_push(xp,yp,zn); }
                       if (_draw_fill_is_inside(x,yp,zn)) { _draw_fill_push(x,yp,zn); }
                       if (xn<width() && _draw_fill_is_inside(xn,yp,zn)) { _draw_fill_push(xn,yp,zn); }
                     }
+
                     if (xp>=0 && _draw_fill_is_inside(xp,y,zn)) { _draw_fill_push(xp,y,zn); }
                     if (xn<width() && _draw_fill_is_inside(xn,y,zn)) { _draw_fill_push(xn,y,zn); }
-                    if (yn<height()) {
+
+                    if (yn<height() && !is_yn) {
                       if (xp>=0 && _draw_fill_is_inside(xp,yn,zn)) { _draw_fill_push(xp,yn,zn); }
                       if (_draw_fill_is_inside(x,yn,zn)) { _draw_fill_push(x,yn,zn); }
                       if (xn<width() && _draw_fill_is_inside(xn,yn,zn)) { _draw_fill_push(xn,yn,zn); }

cf3ca1851e0cf579f10ccdf5c662f153f819119b
dtschump <David.Tschumperle@gmail.com>
2017-05-31 10:26:21 +0200

    Start work for next version 2.0.1.

--- CImg.h
+++ CImg.h
@@ -54,7 +54,7 @@
 
 // Set version number of the library.
 #ifndef cimg_version
-#define cimg_version 200
+#define cimg_version 201
 
 /*-----------------------------------------------------------
  #

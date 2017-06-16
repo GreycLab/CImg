9bece010425184d6793e05e54a7bd2df1cec16f7
David Tschumperlé <David.Tschumperle@gmail.com>
2017-06-16 10:36:15 +0200

    .

--- CImg.h
+++ CImg.h
@@ -121,6 +121,7 @@
 #pragma warning(disable:4244)
 #pragma warning(disable:4311)
 #pragma warning(disable:4312)
+#pragma warning(disable:4319)
 #pragma warning(disable:4512)
 #pragma warning(disable:4571)
 #pragma warning(disable:4640)

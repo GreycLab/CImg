8af9d1b45f717252a92daeb59a53945bb85dd540
David Tschumperlé <David.Tschumperle@gmail.com>
2017-09-05 11:19:48 +0200

    .

--- CImg.h
+++ CImg.h
@@ -4959,30 +4959,25 @@ namespace cimg_library_suffixed {
         case 2 : {
           for (unsigned short *ptr = (unsigned short*)buffer + size; ptr>(unsigned short*)buffer; ) {
             const unsigned short val = *(--ptr);
-            *ptr = (unsigned short)((val>>8)|((val<<8)));
+            *ptr = (unsigned short)((val>>8) | ((val<<8)));
           }
         } break;
         case 4 : {
           for (unsigned int *ptr = (unsigned int*)buffer + size; ptr>(unsigned int*)buffer; ) {
             const unsigned int val = *(--ptr);
-            *ptr = (val>>24)|((val>>8)&0xff00)|((val<<8)&0xff0000)|(val<<24);
+            *ptr = (val>>24) | ((val>>8)&0xff00) | ((val<<8)&0xff0000) | (val<<24);
           }
         } break;
-#if cimg_use_cpp11==1
         case 8 : {
+          const cimg_uint64
+            m0 = (cimg_uint64)0xff, m1 = m0<<8, m2 = m0<<16, m3 = m0<<24,
+            m4 = m0<<32, m5 = m0<<40, m6 = m0<<48, m7 = m0<<56;
           for (cimg_uint64 *ptr = (cimg_uint64*)buffer + size; ptr>(cimg_uint64*)buffer; ) {
             const cimg_uint64 val = *(--ptr);
-            *ptr =  (((val&0xff00000000000000ull)>>56) |
-                     ((val&0x00ff000000000000ull)>>40) |
-                     ((val&0x0000ff0000000000ull)>>24) |
-                     ((val&0x000000ff00000000ull)>>8) |
-                     ((val&0x00000000ff000000ull)<<8) |
-                     ((val&0x0000000000ff0000ull)<<24) |
-                     ((val&0x000000000000ff00ull)<<40) |
-                     ((val&0x00000000000000ffull)<<56));
+            *ptr =  (((val&m7)>>56) | ((val&m6)>>40) | ((val&m5)>>24) | ((val&m4)>>8) |
+                     ((val&m3)<<8) |((val&m2)<<24) | ((val&m1)<<40) | ((val&m0)<<56));
           }
         } break;
-#endif
         default : {
           for (T* ptr = buffer + size; ptr>buffer; ) {
             unsigned char *pb = (unsigned char*)(--ptr), *pe = pb + sizeof(T);

f8ea8677b11ada66e304eb10fa306fcbd4d0fe5b
dtschump <David.Tschumperle@gmail.com>
2017-09-14 16:34:52 +0200

    .

--- CImg.h
+++ CImg.h
@@ -21768,7 +21768,7 @@ namespace cimg_library_suffixed {
       static double mp_list_stats(_cimg_math_parser& mp) {
         const unsigned int
           ind = (unsigned int)cimg::mod((int)_mp_arg(2),mp.listin.width()),
-          k = (unsigned int)_mp_arg(3);
+          k = (unsigned int)mp.opcode[3];
         if (!mp.list_stats) mp.list_stats.assign(mp.listin._width);
         if (!mp.list_stats[ind]) mp.list_stats[ind].assign(1,14,1,1,0).fill(mp.listin[ind].get_stats(),false);
         return mp.list_stats(ind,k);

2008142870495c9ca1402cf71da3129e8c22f95c
dtschump <David.Tschumperle@gmail.com>
2017-09-18 13:39:59 +0200

    .

--- CImg.h
+++ CImg.h
@@ -229,7 +229,7 @@
 
 #else
 
-#if UINTPTR_MAX==0xffffffff || defined(__arm__) || defined(_M_ARM)
+#if UINTPTR_MAX==0xffffffff || defined(__arm__) || defined(_M_ARM) || ((ULONG_MAX)==(UINT_MAX))
 #define cimg_uint64 unsigned long long
 #define cimg_int64 long long
 #define cimg_fuint64 "%llu"
@@ -4954,18 +4954,21 @@ namespace cimg_library_suffixed {
     **/
     template<typename T>
     inline void invert_endianness(T* const buffer, const cimg_ulong size) {
-      if (size) {
-        if (sizeof(T)==2)
+      if (size) switch (sizeof(T)) {
+        case 1 : break;
+        case 2 : {
           for (unsigned short *ptr = (unsigned short*)buffer + size; ptr>(unsigned short*)buffer; ) {
             const unsigned short val = *(--ptr);
             *ptr = (unsigned short)((val>>8) | ((val<<8)));
           }
-        else if (sizeof(T)==4)
+        } break;
+        case 4 : {
           for (unsigned int *ptr = (unsigned int*)buffer + size; ptr>(unsigned int*)buffer; ) {
             const unsigned int val = *(--ptr);
             *ptr = (val>>24) | ((val>>8)&0xff00) | ((val<<8)&0xff0000) | (val<<24);
           }
-        else if (sizeof(T)==8 && sizeof(cimg_uint64)==8) {
+        } break;
+        case 8 : {
           const cimg_uint64
             m0 = (cimg_uint64)0xff, m1 = m0<<8, m2 = m0<<16, m3 = m0<<24,
             m4 = m0<<32, m5 = m0<<40, m6 = m0<<48, m7 = m0<<56;
@@ -4974,12 +4977,14 @@ namespace cimg_library_suffixed {
             *ptr =  (((val&m7)>>56) | ((val&m6)>>40) | ((val&m5)>>24) | ((val&m4)>>8) |
                      ((val&m3)<<8) |((val&m2)<<24) | ((val&m1)<<40) | ((val&m0)<<56));
           }
-        } else if (sizeof(T)!=1)
+        } break;
+        default : {
           for (T* ptr = buffer + size; ptr>buffer; ) {
             unsigned char *pb = (unsigned char*)(--ptr), *pe = pb + sizeof(T);
             for (int i = 0; i<(int)sizeof(T)/2; ++i) swap(*(pb++),*(--pe));
           }
-      }
+        }
+        }
     }
 
     //! Reverse endianness of a single variable.

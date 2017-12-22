f5611c3f1a70a7af66dbc9f5c70c4503205b2a06
dtschump <David.Tschumperle@gmail.com>
2017-12-22 09:30:25 +0100

    .

--- CImg.h
+++ CImg.h
@@ -54,7 +54,7 @@
 
 // Set version number of the library.
 #ifndef cimg_version
-#define cimg_version 217
+#define cimg_version 218
 
 /*-----------------------------------------------------------
  #
@@ -20698,14 +20698,19 @@ namespace cimg_library_suffixed {
       static double mp_echo(_cimg_math_parser& mp) {
         const unsigned int nb_args = (unsigned int)(mp.opcode[2] - 3)/2;
         CImgList<charT> _str;
+        CImg<charT> it;
         for (unsigned int n = 0; n<nb_args; ++n) {
           const unsigned int siz = (unsigned int)mp.opcode[4 + 2*n];
-          if (siz) { // Vector argument
+          if (siz) { // Vector argument -> string
             const double *ptr = &_mp_arg(3 + 2*n) + 1;
             unsigned int l = 0;
             while (l<siz && ptr[l]) ++l;
             CImg<doubleT>(ptr,l,1,1,1,true).move_to(_str);
-          } else CImg<charT>::vector((char)_mp_arg(3 + 2*n)).move_to(_str); // Scalar argument
+          } else { // Scalar argument -> number
+            it.assign(256);
+            cimg_snprintf(it,it._width,"%lf",_mp_arg(3 + 2*n));
+            CImg<charT>::string(it,false,true).move_to(_str);
+          }
         }
         CImg(1,1,1,1,0).move_to(_str);
         const CImg<charT> str = _str>'x';
@@ -20720,14 +20725,19 @@ namespace cimg_library_suffixed {
       static double mp_ext(_cimg_math_parser& mp) {
         const unsigned int nb_args = (unsigned int)(mp.opcode[2] - 3)/2;
         CImgList<charT> _str;
+        CImg<charT> it;
         for (unsigned int n = 0; n<nb_args; ++n) {
           const unsigned int siz = (unsigned int)mp.opcode[4 + 2*n];
-          if (siz) { // Vector argument
+          if (siz) { // Vector argument -> string
             const double *ptr = &_mp_arg(3 + 2*n) + 1;
             unsigned int l = 0;
             while (l<siz && ptr[l]) ++l;
             CImg<doubleT>(ptr,l,1,1,1,true).move_to(_str);
-          } else CImg<charT>::vector((char)_mp_arg(3 + 2*n)).move_to(_str); // Scalar argument
+          } else { // Scalar argument -> number
+            it.assign(256);
+            cimg_snprintf(it,it._width,"%lf",_mp_arg(3 + 2*n));
+            CImg<charT>::string(it,false,true).move_to(_str);
+          }
         }
         CImg(1,1,1,1,0).move_to(_str);
         CImg<charT> str = _str>'x';

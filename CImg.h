64e210ce2e0986ebd6ef7418c349725bf5a3e412
dtschump <David.Tschumperle@gmail.com>
2018-03-15 18:20:04 +0100

    Auto-commit for release 2.2.2_pre

--- CImg.h
+++ CImg.h
@@ -54,7 +54,7 @@
 
 // Set version number of the library.
 #ifndef cimg_version
-#define cimg_version 221
+#define cimg_version 222
 
 /*-----------------------------------------------------------
  #

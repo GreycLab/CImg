56bb030a90e676a45394e49979e028589578fcfb
David Tschumperlé <David.Tschumperle@gmail.com>
2018-03-20 14:53:18 +0100

    .

--- CImg.h
+++ CImg.h
@@ -2764,9 +2764,16 @@ namespace cimg_library_suffixed {
 #endif
       }
       static bool is_nan(const double val) { // Custom version that works with '-ffast-math'
-        cimg_uint64 u;
-        std::memcpy(&u,&val,sizeof(double));
-        return ((unsigned int)(u>>32)&0x7fffffff) + ((unsigned int)u!=0)>0x7ff00000;
+        if (sizeof(double)==8) {
+          cimg_uint64 u;
+          std::memcpy(&u,&val,sizeof(double));
+          return ((unsigned int)(u>>32)&0x7fffffff) + ((unsigned int)u!=0)>0x7ff00000;
+        }
+#ifdef isnan
+        return (bool)isnan(val);
+#else
+        return !(val==val);
+#endif
       }
       static double min() { return -DBL_MAX; }
       static double max() { return DBL_MAX; }
@@ -2801,9 +2808,16 @@ namespace cimg_library_suffixed {
 #endif
       }
       static bool is_nan(const float val) { // Custom version that works with '-ffast-math'
-        unsigned int u;
-        std::memcpy(&u,&val,sizeof(float));
-        return (u&0x7fffffff)>0x7f800000;
+        if (sizeof(float)==4) {
+          unsigned int u;
+          std::memcpy(&u,&val,sizeof(float));
+          return (u&0x7fffffff)>0x7f800000;
+        }
+#ifdef isnan
+        return (bool)isnan(val);
+#else
+        return !(val==val);
+#endif
       }
       static float min() { return -FLT_MAX; }
       static float max() { return FLT_MAX; }

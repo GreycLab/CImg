6d7eb33c42dc1f91a9bb6e97b7b4db40d2eb71bb
dtschump <David.Tschumperle@gmail.com>
2018-03-28 11:14:54 +0200

    .

--- CImg.h
+++ CImg.h
@@ -2868,7 +2868,12 @@ namespace cimg_library_suffixed {
         return !is_nan(val) && (val<cimg::type<half>::min() || val>cimg::type<half>::max());
 #endif
       }
-      static bool is_nan(const half val) {
+      static bool is_nan(const half val) { // Custom version that works with '-ffast-math'
+        if (sizeof(half)==2) {
+          short u;
+          std::memcpy(&u,&val,sizeof(short));
+          return (bool)((u&0x7fff)>0x7c00);
+        }
         return cimg::type<float>::is_nan((float)val);
       }
       static half min() { return (half)-65504; }

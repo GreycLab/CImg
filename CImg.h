5bb8a03d7fed06275ddb53a56c567fb6f61aa4a4
David Tschumperlé <David.Tschumperle@gmail.com>
2018-07-26 14:07:33 +0200

    .

--- CImg.h
+++ CImg.h
@@ -20559,7 +20559,7 @@ namespace cimg_library_suffixed {
           dy = (unsigned int)mp.opcode[8],
           dz = (unsigned int)mp.opcode[9],
           dc = (unsigned int)mp.opcode[10];
-        const bool boundary_conditions = (bool)_mp_arg(11);
+        const unsigned int boundary_conditions = (unsigned int)_mp_arg(11);
         unsigned int ind = (unsigned int)mp.opcode[2];
         if (ind!=~0U) ind = (unsigned int)cimg::mod((int)_mp_arg(2),mp.listin.width());
         const CImg<T> &img = ind==~0U?mp.imgin:mp.listin[ind];

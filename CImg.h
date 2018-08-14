1979f14108ce1d209ac598f8cd5047f7315c6616
David Tschumperlé <David.Tschumperle@gmail.com>
2018-08-14 09:26:50 +0200

    .

--- CImg.h
+++ CImg.h
@@ -19791,7 +19791,7 @@ namespace cimg_library_suffixed {
         CImg<charT> res;
         if (_cimg_mp_is_vector(arg)) { // Vector
           CImg<charT>::string("vectorXXXXXXXXXXXXXXXX").move_to(res);
-          std::sprintf(res._data + 6,"%u",_cimg_mp_size(arg));
+          cimg_sprintf(res._data + 6,"%u",_cimg_mp_size(arg));
         } else CImg<charT>::string("scalar").move_to(res);
         return res;
       }

c00563fc9ee822bb2e3755ffa906717556a40235
David Tschumperlé <David.Tschumperle@gmail.com>
2019-01-03 08:41:24 +0100

    .

--- CImg.h
+++ CImg.h
@@ -5802,7 +5802,7 @@ namespace cimg_library_suffixed {
     }
 
     // Custom random number generator (allow re-entrance).
-    inline cimg_ulong *__rand() {
+    inline cimg_ulong *__rand() { // Used as a shared global rng
       static cimg_ulong rng = 0xB16B00B5U;
       return &rng;
     }
@@ -6848,13 +6848,12 @@ namespace cimg_library_suffixed {
     // Return a random filename.
     inline const char* filenamerand() {
       cimg::mutex(6);
-      cimg_ulong rng = cimg::srand();
       static char randomid[9];
       for (unsigned int k = 0; k<8; ++k) {
-        const int v = (int)cimg::rand(65535,&rng)%3;
-        randomid[k] = (char)(v==0?('0' + ((int)cimg::rand(65535,&rng)%10)):
-                             (v==1?('a' + ((int)cimg::rand(65535,&rng)%26)):
-                              ('A' + ((int)cimg::rand(65535,&rng)%26))));
+        const int v = (int)cimg::rand(65535)%3;
+        randomid[k] = (char)(v==0?('0' + ((int)cimg::rand(65535)%10)):
+                             (v==1?('a' + ((int)cimg::rand(65535)%26)):
+                              ('A' + ((int)cimg::rand(65535)%26))));
       }
       cimg::mutex(6,0);
       return randomid;

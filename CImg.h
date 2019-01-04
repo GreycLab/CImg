8e5d274040a10a01b190f47135dcf9032728a09e
David Tschumperlé <David.Tschumperle@gmail.com>
2019-01-04 08:52:23 +0100

    .

--- CImg.h
+++ CImg.h
@@ -5776,12 +5776,12 @@ namespace cimg_library_suffixed {
 #endif
     }
 
-    inline unsigned int wait(const unsigned int milliseconds, cimg_ulong *const timer) {
-      if (!*timer) *timer = cimg::time();
+    inline unsigned int wait(const unsigned int milliseconds, cimg_ulong *const p_timer) {
+      if (!*p_timer) *p_timer = cimg::time();
       const cimg_ulong current_time = cimg::time();
-      if (current_time>=*timer + milliseconds) { *timer = current_time; return 0; }
-      const unsigned int time_diff = (unsigned int)(*timer + milliseconds - current_time);
-      *timer = current_time + time_diff;
+      if (current_time>=*p_timer + milliseconds) { *p_timer = current_time; return 0; }
+      const unsigned int time_diff = (unsigned int)(*p_timer + milliseconds - current_time);
+      *p_timer = current_time + time_diff;
       cimg::sleep(time_diff);
       return time_diff;
     }

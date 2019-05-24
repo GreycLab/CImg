b638ce06605fd78eb00b8256a0f486dfddfbbef5
David Tschumperlé <david.tschumperle@gmail.com>
2019-05-24 11:51:21 +0200

    .

--- CImg.h
+++ CImg.h
@@ -26255,7 +26255,7 @@ namespace cimg_library_suffixed {
           _mp = omp_get_thread_num()?mp:_cimg_math_parser(),
           &lmp = omp_get_thread_num()?_mp:mp;
         cimg_pragma_openmp(for)
-          for (unsigned int i = 0; i<res._height; ++i) {
+          for (int i = 0; i<res.height(); ++i) {
             const unsigned int i4 = 4*i;
             const double
               x = (double)xyzc[i4], y = (double)xyzc[i4 + 1],
@@ -29690,27 +29690,27 @@ namespace cimg_library_suffixed {
         switch (boundary_conditions) {
         case 3 : // Mirror
           cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-          for (ulongT off = 0; off<siz; ++off) {
+          for (longT off = 0; off<(longT)siz; ++off) {
             const ulongT ind = ((ulongT)_data[off])%cwhd2;
             res[off] = colormap[ind<cwhd?ind:cwhd2 - ind - 1];
           }
           break;
         case 2 : // Periodic
           cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-          for (ulongT off = 0; off<siz; ++off) {
+          for (longT off = 0; off<(longT)siz; ++off) {
             const ulongT ind = (ulongT)_data[off];
             res[off] = colormap[ind%cwhd];
           }
           break;
         case 1 : // Neumann
           cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-          for (ulongT off = 0; off<siz; ++off) {
+          for (longT off = 0; off<(longT)siz; ++off) {
             const longT ind = (longT)_data[off];
             res[off] = colormap[cimg::cut(ind,(longT)0,(longT)cwhd - 1)];
           } break;
         default : // Dirichlet
           cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-          for (ulongT off = 0; off<siz; ++off) {
+          for (longT off = 0; off<(longT)siz; ++off) {
             const ulongT ind = (ulongT)_data[off];
             res[off] = ind<cwhd?colormap[ind]:(t)0;
           }
@@ -29725,7 +29725,7 @@ namespace cimg_library_suffixed {
             t *const ptrd0 = res.data(0,0,0,2*c), *const ptrd1 = ptrd0 + whd;
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const ulongT
                 _ind = ((ulongT)ptrs[off])%cwhd2,
                 ind = _ind<cwhd?_ind:cwhd2 - _ind - 1;
@@ -29737,7 +29737,7 @@ namespace cimg_library_suffixed {
             t *const ptrd0 = res.data(0,0,0,2*c), *const ptrd1 = ptrd0 + whd;
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const ulongT ind = ((ulongT)ptrs[off])%cwhd;
               ptrd0[off] = ptrp0[ind]; ptrd1[off] = ptrp1[ind];
             }
@@ -29747,7 +29747,7 @@ namespace cimg_library_suffixed {
             t *const ptrd0 = res.data(0,0,0,2*c), *const ptrd1 = ptrd0 + whd;
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const longT ind = cimg::cut((longT)ptrs[off],(longT)0,(longT)cwhd - 1);
               ptrd0[off] = ptrp0[ind]; ptrd1[off] = ptrp1[ind];
             }
@@ -29757,7 +29757,7 @@ namespace cimg_library_suffixed {
             t *const ptrd0 = res.data(0,0,0,2*c), *const ptrd1 = ptrd0 + whd;
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const ulongT ind = (ulongT)ptrs[off];
               if (ind<cwhd) { ptrd0[off] = ptrp0[ind]; ptrd1[off] = ptrp1[ind]; }
               else ptrd0[off] = ptrd1[off] = (t)0;
@@ -29774,7 +29774,7 @@ namespace cimg_library_suffixed {
             t *const ptrd0 = res.data(0,0,0,3*c), *const ptrd1 = ptrd0 + whd, *const ptrd2 = ptrd1 + whd;
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const ulongT
                 _ind = ((ulongT)ptrs[off])%cwhd2,
                 ind = _ind<cwhd?_ind:cwhd2 - _ind - 1;
@@ -29786,7 +29786,7 @@ namespace cimg_library_suffixed {
             t *const ptrd0 = res.data(0,0,0,3*c), *const ptrd1 = ptrd0 + whd, *const ptrd2 = ptrd1 + whd;
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const ulongT ind = ((ulongT)ptrs[off])%cwhd;
               ptrd0[off] = ptrp0[ind]; ptrd1[off] = ptrp1[ind]; ptrd2[off] = ptrp2[ind];
             }
@@ -29796,7 +29796,7 @@ namespace cimg_library_suffixed {
             t *const ptrd0 = res.data(0,0,0,3*c), *const ptrd1 = ptrd0 + whd, *const ptrd2 = ptrd1 + whd;
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const longT ind = cimg::cut((longT)ptrs[off],(longT)0,(longT)cwhd - 1);
               ptrd0[off] = ptrp0[ind]; ptrd1[off] = ptrp1[ind]; ptrd2[off] = ptrp2[ind];
             }
@@ -29806,7 +29806,7 @@ namespace cimg_library_suffixed {
             t *const ptrd0 = res.data(0,0,0,3*c), *const ptrd1 = ptrd0 + whd, *const ptrd2 = ptrd1 + whd;
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const ulongT ind = (ulongT)ptrs[off];
               if (ind<cwhd) { ptrd0[off] = ptrp0[ind]; ptrd1[off] = ptrp1[ind]; ptrd2[off] = ptrp2[ind]; }
               else ptrd0[off] = ptrd1[off] = ptrd2[off] = (t)0;
@@ -29822,7 +29822,7 @@ namespace cimg_library_suffixed {
             t *const ptrd = res.data(0,0,0,colormap._spectrum*c);
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const ulongT
                 _ind = ((ulongT)ptrs[off])%cwhd,
                 ind = _ind<cwhd?_ind:cwhd2 - _ind - 1;
@@ -29836,7 +29836,7 @@ namespace cimg_library_suffixed {
             t *const ptrd = res.data(0,0,0,colormap._spectrum*c);
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const ulongT ind = ((ulongT)ptrs[off])%cwhd;
               t *const _ptrd = ptrd + off;
               const t *const ptrp = &colormap[ind];
@@ -29848,7 +29848,7 @@ namespace cimg_library_suffixed {
             t *const ptrd = res.data(0,0,0,colormap._spectrum*c);
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const longT ind = cimg::cut((longT)ptrs[off],(longT)0,(longT)cwhd - 1);
               t *const _ptrd = ptrd + off;
               const t *const ptrp = &colormap[ind];
@@ -29860,7 +29860,7 @@ namespace cimg_library_suffixed {
             t *const ptrd = res.data(0,0,0,colormap._spectrum*c);
             const T *const ptrs = data(0,0,0,c);
             cimg_pragma_openmp(parallel for cimg_openmp_if_size(size(),256))
-            for (ulongT off = 0; off<whd; ++off) {
+            for (longT off = 0; off<(longT)whd; ++off) {
               const ulongT ind = (ulongT)ptrs[off];
               t *const _ptrd = ptrd + off;
               if (ind<cwhd) {

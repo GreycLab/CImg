9db8b7b687fd61cae15c8785f584708440426c38
David Tschumperlé <david.tschumperle@gmail.com>
2019-06-17 15:26:30 +0200

    .

--- CImg.h
+++ CImg.h
@@ -42485,16 +42485,15 @@ namespace cimg_library_suffixed {
       cimg_init_scanline(opacity);
 
       const int
-        hdy01 = dy01/2, step = y0<=y1?1:-1,
+        step = y0<=y1?1:-1,
+        hdy01 = dy01*step/2,
         cy0 = cimg::cut(y0,0,h1), cy1 = cimg::cut(y1,0,h1) + step;
       dy01+=dy01?0:1;
 
       for (int y = cy0; y!=cy1; y+=step) {
         const int
           yy0 = y - y0,
-//          x = x0 + (dx01*yy0 + hdy01)/dy01;
-//          x = cimg::round(x0 + (float)dx01*yy0/dy01);
-          x = x0 + (dx01*yy0 + hdy01)/dy01;
+          x = x0 + (dx01*yy0 - hdy01)/dy01;
         if (x>=0 && x<=w1 && pattern&hatch) {
           T *const ptrd = is_horizontal?data(y,x):data(x,y);
           cimg_forC(*this,c) {

d80358b74deebc5f741fb1901c2251cdfb945abc
David Tschumperlé <david.tschumperle@gmail.com>
2019-07-02 14:45:52 +0200

    .

--- CImg.h
+++ CImg.h
@@ -44745,26 +44745,6 @@ namespace cimg_library_suffixed {
       return *this;
     }
 
-    //! Draw an outlined 3D rectangle \overloading.
-    template<typename tc>
-    CImg<T>& draw_rectangle(const int x0, const int y0, const int z0,
-                            const int x1, const int y1, const int z1,
-                            const tc *const color, const float opacity,
-                            const unsigned int pattern) {
-      return draw_line(x0,y0,z0,x1,y0,z0,color,opacity,pattern,true).
-	draw_line(x1,y0,z0,x1,y1,z0,color,opacity,pattern,false).
-	draw_line(x1,y1,z0,x0,y1,z0,color,opacity,pattern,false).
-	draw_line(x0,y1,z0,x0,y0,z0,color,opacity,pattern,false).
-	draw_line(x0,y0,z1,x1,y0,z1,color,opacity,pattern,true).
-	draw_line(x1,y0,z1,x1,y1,z1,color,opacity,pattern,false).
-	draw_line(x1,y1,z1,x0,y1,z1,color,opacity,pattern,false).
-	draw_line(x0,y1,z1,x0,y0,z1,color,opacity,pattern,false).
-	draw_line(x0,y0,z0,x0,y0,z1,color,opacity,pattern,true).
-	draw_line(x1,y0,z0,x1,y0,z1,color,opacity,pattern,true).
-	draw_line(x1,y1,z0,x1,y1,z1,color,opacity,pattern,true).
-	draw_line(x0,y1,z0,x0,y1,z1,color,opacity,pattern,true);
-    }
-
     //! Draw a filled 2D rectangle.
     /**
        \param x0 X-coordinate of the upper-left rectangle corner.

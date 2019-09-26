a9b70da58a337231d704f63a896a3e0d1ad23e84
David Tschumperlé <david.tschumperle@gmail.com>
2019-09-26 15:41:26 +0200

    .

--- CImg.h
+++ CImg.h
@@ -9702,35 +9702,23 @@ namespace cimg_library_suffixed {
       const unsigned int sx = screen_width(), sy = screen_height();
       if (sx==_width && sy==_height) return;
       XSetWindowAttributes attr_set;
+
+
+      attr_set.background_pixel = XBlackPixel(dpy,XDefaultScreen(dpy));
       attr_set.override_redirect = 1;
       _background_window = XCreateWindow(dpy,DefaultRootWindow(dpy),0,0,sx,sy,0,0,
-                                         InputOutput,CopyFromParent,CWOverrideRedirect,&attr_set);
-
-      const cimg_ulong buf_size = (cimg_ulong)sx*sy*(cimg::X11_attr().nb_bits==8?1:
-                                                     (cimg::X11_attr().nb_bits==16?2:4));
-      void *background_data = std::malloc(buf_size);
-      std::memset(background_data,0,buf_size);
-      XImage *background_image = XCreateImage(dpy,DefaultVisual(dpy,DefaultScreen(dpy)),cimg::X11_attr().nb_bits,
-                                              ZPixmap,0,(char*)background_data,sx,sy,8,0);
+                                         InputOutput,CopyFromParent,CWBackPixel | CWOverrideRedirect,&attr_set);
       XEvent event;
       XSelectInput(dpy,_background_window,StructureNotifyMask);
       XMapRaised(dpy,_background_window);
       do XWindowEvent(dpy,_background_window,StructureNotifyMask,&event);
       while (event.type!=MapNotify);
-      GC gc = DefaultGC(dpy,DefaultScreen(dpy));
 
-#ifdef cimg_use_xshm
-      if (_shminfo) XShmPutImage(dpy,_background_window,gc,background_image,0,0,0,0,sx,sy,0);
-      else XPutImage(dpy,_background_window,gc,background_image,0,0,0,0,sx,sy);
-#else
-      XPutImage(dpy,_background_window,gc,background_image,0,0,0,0,sx,sy);
-#endif
       XWindowAttributes attr;
       do {
         XGetWindowAttributes(dpy,_background_window,&attr);
         if (attr.map_state!=IsViewable) { XSync(dpy,0); cimg::sleep(10); }
       } while (attr.map_state!=IsViewable);
-      XDestroyImage(background_image);
     }
 
     void _desinit_fullscreen() {

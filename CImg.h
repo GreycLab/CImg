97f185925d1f3fb3b8f7b64c6f78ecabd5acbe37
David Tschumperlé <david.tschumperle@gmail.com>
2019-10-02 13:48:38 +0200

    .

--- CImg.h
+++ CImg.h
@@ -7653,7 +7653,7 @@ namespace cimg_library_suffixed {
     }
 
     inline void sgels(char & TRANS, int &M, int &N, int &NRHS, float* lapA, int &LDA,
-		      float* lapB, int &LDB, float* WORK, int &LWORK, int &INFO){
+		      float* lapB, int &LDB, float* WORK, int &LWORK, int &INFO) {
       sgels_(&TRANS, &M, &N, &NRHS, lapA, &LDA, lapB, &LDB, WORK, &LWORK, &INFO);
     }
 

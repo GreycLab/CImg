aa0d98df1b9d57fe27754a663875f11e378c3e05
David Tschumperlé <David.Tschumperle@gmail.com>
2019-12-13 17:36:19 +0100

    .

--- CImg.h
+++ CImg.h
@@ -16690,7 +16690,7 @@ namespace cimg_library_suffixed {
         //    1 = compile-time constant | N>1 = constant ptr to vector[N-1] }.
         memtype.assign(mem._width,1,1,1,0);
         for (unsigned int i = 0; i<_cimg_mp_slot_x; ++i) memtype[i] = 1;
-        memtype[17] = 0;
+        memtype[17] = -1;
         memtype[_cimg_mp_slot_x] = memtype[_cimg_mp_slot_y] = memtype[_cimg_mp_slot_z] = memtype[_cimg_mp_slot_c] = -2;
         mempos = _cimg_mp_slot_c + 1;
         variable_pos.assign(8);

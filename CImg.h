a2265de8a7d5d351391df93d4ed1b6296eac2f2d
David Tschumperlé <david.tschumperle@gmail.com>
2020-01-28 13:51:48 +0100

    Auto-commit for release 2.8.4_pre

--- CImg.h
+++ CImg.h
@@ -54,7 +54,7 @@
 
 // Set version number of the library.
 #ifndef cimg_version
-#define cimg_version 283
+#define cimg_version 284
 
 /*-----------------------------------------------------------
  #

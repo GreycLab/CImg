98ed52ac967bc0cd0df252418d592655c58486be
David Tschumperlé <David.Tschumperle@gmail.com>
2020-02-23 11:40:33 +0100

    .

--- CImg.h
+++ CImg.h
@@ -22956,9 +22956,9 @@ namespace cimg_library_suffixed {
       }
 
       static double mp_find(_cimg_math_parser& mp) {
-        const bool is_forward = (bool)_mp_arg(6);
+        const int _step = (int)_mp_arg(6), step = _step?_step:-1;
         const ulongT siz = (ulongT)mp.opcode[3];
-        longT ind = (longT)(mp.opcode[5]!=_cimg_mp_slot_nan?_mp_arg(5):is_forward?0:siz - 1);
+        longT ind = (longT)(mp.opcode[5]!=_cimg_mp_slot_nan?_mp_arg(5):step>0?0:siz - 1);
         if (ind<0 || ind>=(longT)siz) return -1.;
         const double
           *const ptrb = &_mp_arg(2) + 1,
@@ -22967,13 +22967,13 @@ namespace cimg_library_suffixed {
           *ptr = ptrb + ind;
 
         // Forward search
-        if (is_forward) {
-          while (ptr<ptre && *ptr!=val) ++ptr;
-          return ptr==ptre?-1.:(double)(ptr - ptrb);
+        if (step>0) {
+          while (ptr<ptre && *ptr!=val) ptr+=step;
+          return ptr>=ptre?-1.:(double)(ptr - ptrb);
         }
 
         // Backward search.
-        while (ptr>=ptrb && *ptr!=val) --ptr;
+        while (ptr>=ptrb && *ptr!=val) ptr-=step;
         return ptr<ptrb?-1.:(double)(ptr - ptrb);
       }
 

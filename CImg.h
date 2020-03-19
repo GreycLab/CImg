3fec22215c47454155f7c4afc59c1647d51e6a55
David Tschumperlé <David.Tschumperle@gmail.com>
2020-03-19 22:16:00 +0100

    .

--- CImg.h
+++ CImg.h
@@ -7335,14 +7335,14 @@ namespace cimg_library_suffixed {
     **/
     inline const char *split_filename(const char *const filename, char *const body=0) {
       if (!filename) { if (body) *body = 0; return 0; }
-      const char *p = 0; for (const char *np = filename; np>=filename && (p=np); np = std::strchr(np,'.') + 1) {}
-      if (p==filename || std::strchr(p,'/') || std::strchr(p,'\\')) {
+      const char * p = std::strrchr(filename,'.');
+      if (!p || std::strchr(p,'/') || std::strchr(p,'\\')) { // No extension.
         if (body) std::strcpy(body,filename);
         return filename + std::strlen(filename);
       }
-      const unsigned int l = (unsigned int)(p - filename - 1);
+      const unsigned int l = (unsigned int)(p - filename);
       if (body) { if (l) std::memcpy(body,filename,l); body[l] = 0; }
-      return p;
+      return p + 1;
     }
 
     // Generate a numbered version of a filename.

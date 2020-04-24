b99dd215ce382e62eda0d659a731d934143af559
David Tschumperlé <David.Tschumperle@gmail.com>
2020-04-24 16:06:44 +0200

    .

--- CImg.h
+++ CImg.h
@@ -28178,29 +28178,31 @@ namespace cimg_library_suffixed {
         const double a = (double)A[0], b = (double)A[1], c = (double)A[2], d = (double)A[3],
           fa = std::fabs(a), fb = std::fabs(b), fc = std::fabs(c), fd = std::fabs(d),
           det = a*d - b*c, fM = cimg::max(fa,fb,fc,fd);
-        if (fM==fa) cimg_forX(*this,k) {
+        if (fM==fa)
+          cimg_pragma_openmp(parallel for cimg_openmp_if(_width>=256))
+          cimg_forX(*this,k) {
             const double u = (double)(*this)(k,0), v = (double)(*this)(k,1), y = (a*v - c*u)/det;
             (*this)(k,0) = (T)((u - b*y)/a); (*this)(k,1) = (T)y;
-          } else if (fM==fc) cimg_forX(*this,k) {
+          } else if (fM==fc)
+          cimg_pragma_openmp(parallel for cimg_openmp_if(_width>=256))
+          cimg_forX(*this,k) {
             const double u = (double)(*this)(k,0), v = (double)(*this)(k,1), y = (a*v - c*u)/det;
             (*this)(k,0) = (T)((v - d*y)/c); (*this)(k,1) = (T)y;
-          } else if (fM==fb) cimg_forX(*this,k) {
+          } else if (fM==fb)
+          cimg_pragma_openmp(parallel for cimg_openmp_if(_width>=256))
+          cimg_forX(*this,k) {
             const double u = (double)(*this)(k,0), v = (double)(*this)(k,1), x = (d*u - b*v)/det;
             (*this)(k,0) = (T)x; (*this)(k,1) = (T)((u - a*x)/b);
-          } else cimg_forX(*this,k) {
+          } else
+          cimg_pragma_openmp(parallel for cimg_openmp_if(_width>=256))
+          cimg_forX(*this,k) {
             const double u = (double)(*this)(k,0), v = (double)(*this)(k,1), x = (d*u - b*v)/det;
             (*this)(k,0) = (T)x; (*this)(k,1) = (T)((v - c*x)/d);
           }
         return *this;
       }
-      if (_width!=1) { // Process column-by-column
-        CImg<T> res(_width,A._width);
-        cimg_forX(*this,i) res.draw_image(i,get_column(i).solve(A));
-        return res.move_to(*this);
-      }
 
       if (A._width==A._height) { // Square linear system
-
 #ifdef cimg_use_lapack
         char TRANS = 'N';
         int INFO, N = _height, LWORK = 4*N, *const IPIV = new int[N];
@@ -28209,30 +28211,37 @@ namespace cimg_library_suffixed {
           *const lapB = new Ttfloat[N],
           *const WORK = new Ttfloat[LWORK];
         cimg_forXY(A,k,l) lapA[k*N + l] = (Ttfloat)(A(k,l));
-        cimg_forY(*this,i) lapB[i] = (Ttfloat)((*this)(i));
-        cimg::getrf(N,lapA,IPIV,INFO);
-        if (INFO)
-          cimg::warn(_cimg_instance
-                     "solve(): LAPACK library function dgetrf_() returned error code %d.",
-                     cimg_instance,
-                     INFO);
-
-        if (!INFO) {
-          cimg::getrs(TRANS,N,lapA,IPIV,lapB,INFO);
+        cimg_forX(*this,i) {
+          cimg_forY(*this,j) lapB[j] = (Ttfloat)((*this)(i,j));
+          cimg::getrf(N,lapA,IPIV,INFO);
           if (INFO)
             cimg::warn(_cimg_instance
-                       "solve(): LAPACK library function dgetrs_() returned error code %d.",
+                       "solve(): LAPACK library function dgetrf_() returned error code %d.",
                        cimg_instance,
                        INFO);
+          if (!INFO) {
+            cimg::getrs(TRANS,N,lapA,IPIV,lapB,INFO);
+            if (INFO)
+              cimg::warn(_cimg_instance
+                         "solve(): LAPACK library function dgetrs_() returned error code %d.",
+                         cimg_instance,
+                         INFO);
+          }
+          if (!INFO) cimg_forY(*this,j) (*this)(i,j) = (T)(lapB[j]); else cimg_forY(*this,i,j) (*this)(i,j) = (T)0;
         }
-        if (!INFO) cimg_forY(*this,i) (*this)(i) = (T)(lapB[i]); else fill(0);
         delete[] IPIV; delete[] lapA; delete[] lapB; delete[] WORK;
 #else
         CImg<Ttfloat> lu(A,false);
         CImg<Ttfloat> indx;
         bool d;
         lu._LU(indx,d);
-        _solve(lu,indx);
+        if (_width*_height>=64) {
+          CImg<T> res(_width,A._width);
+          cimg_pragma_openmp(parallel for)
+            cimg_forX(*this,i) res.draw_image(i,get_column(i)._solve(lu,indx));
+          res.move_to(*this);
+        }
+        else _solve(lu,indx);
 #endif
       } else { // Least-square solution for non-square systems
 

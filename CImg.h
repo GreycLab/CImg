8c68c87f03e739ce7d551a8d5153a9539e2a3739
David Tschumperlé <David.Tschumperle@gmail.com>
2020-05-09 14:49:13 +0200

    .

--- CImg.h
+++ CImg.h
@@ -17695,7 +17695,6 @@ namespace cimg_library_suffixed {
               }
             }
 
-            // Write computed value back in image if necessary.
             if (*ref==4) { // Image value (vector): I/J[_#ind,off] **= value
               if (!is_single) is_parallelizable = false;
               p1 = ref[1]; // Index
@@ -21087,14 +21086,11 @@ namespace cimg_library_suffixed {
 
             if (!std::strncmp(ss,"swap(",5)) { // Swap values
               _cimg_mp_op("Function 'swap()'");
-              s1 = ss5; while (s1<se1 && (*s1!=',' || level[s1 - expr._data]!=clevel1)) ++s1;
-              arg1 = compile(ss5,s1,depth1,0,is_single);
-              arg2 = compile(++s1,se1,depth1,0,is_single);
-              p1 = _cimg_mp_size(arg1);
-              p2 = _cimg_mp_size(arg2);
-              _cimg_mp_check_type(arg2,2,p1>0?2:1,p1);
-              CImg<ulongT>::vector((ulongT)mp_swap,arg1,arg2,p1,p2).move_to(code);
-              _cimg_mp_return(arg1);
+              // Small hack : insert macro 'swap()'
+              CImg<char>("swap\0\2",6,1,1,1,true).move_to(macro_def);
+              CImg<char>::string("tmp=\1;\1=\2;\2=tmp").move_to(macro_body);
+              CImg<bool>(macro_body.back().width(),1,1,1,false).move_to(macro_body_is_string);
+              // Do not return to allow further macro evaluation.
             }
             break;
 
@@ -25676,18 +25672,6 @@ namespace cimg_library_suffixed {
         double val = _mp_arg(3);
         for (unsigned int i = 4; i<i_end; ++i) val+=_mp_arg(i);
         return val;
-      }
-
-      static double mp_swap(_cimg_math_parser& mp) {
-        const unsigned int siz = (unsigned int)mp.opcode[3];
-        if (!siz) { // Scalar
-          double &arg1 = _mp_arg(1), &arg2 = _mp_arg(2);
-          cimg::swap(arg1,arg2);
-        } else { // Vector
-          double *ptr1 = &_mp_arg(1) + 1, *ptr2 = &_mp_arg(2) + 1;
-          for (unsigned int k = 0; k<siz; ++k) cimg::swap(ptr1[k],ptr2[k]);
-        }
-        return _mp_arg(1);
       }
 
       static double mp_tan(_cimg_math_parser& mp) {

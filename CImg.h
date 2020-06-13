e49345503fcfa5498d3d2cd2622c162e3b0abed7
David Tschumperlé <David.Tschumperle@gmail.com>
2020-06-13 22:28:06 +0200

    Fix compilation issue with CImg::solve() when Lapack is enabled.

--- CImg.h
+++ CImg.h
@@ -28530,7 +28530,7 @@ namespace cimg_library_suffixed {
                        "solve(): LAPACK library function dgetrf_() returned error code %d.",
                        cimg_instance,
                        INFO);
-          if (!INFO) {
+          else {
             cimg::getrs(TRANS,N,lapA,IPIV,lapB,INFO);
             if (INFO)
               cimg::warn(_cimg_instance
@@ -28538,7 +28538,7 @@ namespace cimg_library_suffixed {
                          cimg_instance,
                          INFO);
           }
-          if (!INFO) cimg_forY(*this,j) (*this)(i,j) = (T)(lapB[j]); else cimg_forY(*this,i,j) (*this)(i,j) = (T)0;
+          if (!INFO) cimg_forY(*this,j) (*this)(i,j) = (T)(lapB[j]); else cimg_forY(*this,j) (*this)(i,j) = (T)0;
         }
         delete[] IPIV; delete[] lapA; delete[] lapB; delete[] WORK;
 #else

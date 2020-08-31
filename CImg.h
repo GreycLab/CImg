914827f77f52a24ae05da7fdd45e097341b2d1d3
David Tschumperlé <David.Tschumperle@gmail.com>
2020-08-31 11:28:39 +0200

    .

--- CImg.h
+++ CImg.h
@@ -28224,17 +28224,9 @@ namespace cimg_library_suffixed {
     **/
     template<typename t>
     double dot(const CImg<t>& img) const {
-      if (is_empty())
-        throw CImgInstanceException(_cimg_instance
-                                    "dot(): Empty instance.",
-                                    cimg_instance);
-      if (!img)
-        throw CImgArgumentException(_cimg_instance
-                                    "dot(): Empty specified image.",
-                                    cimg_instance);
-
       const ulongT nb = std::min(size(),img.size());
       double res = 0;
+      cimg_pragma_openmp(parallel for reduction(+:res) cimg_openmp_if_size(nb,8192))
       for (ulongT off = 0; off<nb; ++off) res+=(double)_data[off]*(double)img[off];
       return res;
     }

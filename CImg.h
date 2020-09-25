24355ac0e2db37bc60ec4e9ba0e65db4603d74dc
David Tschumperlé <david.tschumperle@gmail.com>
2020-09-25 11:14:01 +0200

    .

--- CImg.h
+++ CImg.h
@@ -7836,7 +7836,7 @@ namespace cimg_library_suffixed {
 
     template<typename T>
     inline void sgels(char & TRANS, int &M, int &N, int &NRHS, T* lapA, int &LDA,
-                      T* lapB, int &LDB, T* WORK, int &LWORK, int &INFO){
+                      T* lapB, int &LDB, T* WORK, int &LWORK, int &INFO) {
       dgels_(&TRANS, &M, &N, &NRHS, lapA, &LDA, lapB, &LDB, WORK, &LWORK, &INFO);
     }
 

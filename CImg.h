38ae4f60fe8a837bfc599a8f69fa211c4e8092a8
David Tschumperlé <David.Tschumperle@gmail.com>
2020-10-06 10:10:19 +0200

    .

--- CImg.h
+++ CImg.h
@@ -47904,7 +47904,7 @@ namespace cimg_library_suffixed {
       const unsigned int text_length = (unsigned int)std::strlen(text);
       const int padding_x = font[0]._height<=48?1U:font[0]._height<=128?2U:3U;
       t o_right_opacity, right_opacity, left_opacity;
-      unsigned char o_ch, ch;
+      unsigned char o_ch, ch = 0;
       int x, y, w;
 
       if (is_empty()) {
@@ -47951,8 +47951,8 @@ namespace cimg_library_suffixed {
         assign(x0 + w,y0 + y,1,is_native_font?1:font[0]._spectrum,(T)0);
       }
 
-      o_right_opacity = 0;
-      o_ch = 0;
+      o_right_opacity = right_opacity = 0;
+      o_ch = ch = 0;
       x = x0;
       y = y0;
       for (unsigned int i = 0; i<text_length; ++i) {

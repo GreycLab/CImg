dd8888fe67ea1d7fc983b74d27f86f73639a9d69
David Tschumperlé <david.tschumperle@gmail.com>
2021-05-12 14:00:02 +0200

    Auto-commit for release 2.9.8_pre

--- CImg.h
+++ CImg.h
@@ -21670,7 +21670,7 @@ namespace cimg_library_suffixed {
               (l_opcode>'y').move_to(opcode);
               opcode[2] = opcode._height;
               opcode.move_to(code);
-              is_new_vector = true;
+//              is_new_vector = true;
               _cimg_mp_return(pos);
             }
 
@@ -22058,7 +22058,7 @@ namespace cimg_library_suffixed {
             (l_opcode>'y').move_to(opcode);
             opcode[2] = opcode._height;
             opcode.move_to(code);
-            is_new_vector = true;
+//            is_new_vector = true;
           }
           _cimg_mp_return(pos);
         }

48173196c9e665671dd92926cfd07cf6ef150f62
David Tschumperlé <david.tschumperle@gmail.com>
2021-09-08 10:38:13 +0200

    Fix a few int-overflow bug that could happen in draw_line().

--- CImg.h
+++ CImg.h
@@ -45705,11 +45705,10 @@ namespace cimg_library_suffixed {
 
       if (std::min(y0,y1)>=height() || std::max(y0,y1)<0 || std::min(x0,x1)>=width() || std::max(x0,x1)<0) return *this;
 
-      int
-        w1 = width() - 1, h1 = height() - 1,
-        dx01 = x1 - x0, dy01 = y1 - y0;
-      int
-        dtx01 = tx1 - tx0, dty01 = ty1 - ty0;
+      int w1 = width() - 1, h1 = height() - 1;
+      longT
+        dx01 = (longT)x1 - x0, dy01 = (longT)y1 - y0,
+        dtx01 = (longT)tx1 - tx0, dty01 = (longT)ty1 - ty0;
 
       const bool is_horizontal = cimg::abs(dx01)>cimg::abs(dy01);
       if (is_horizontal) cimg::swap(x0,y0,x1,y1,w1,h1,dx01,dy01);
@@ -45723,15 +45722,17 @@ namespace cimg_library_suffixed {
       if (init_hatch) hatch = ~0U - (~0U>>1);
       cimg_init_scanline(opacity);
 
-      const int
-        step = y0<=y1?1:-1, hdy01 = dy01*cimg::sign(dx01)/2,
-        hdy01tx = dy01*cimg::sign(dtx01)/2, hdy01ty = dy01*cimg::sign(dty01)/2,
-        cy0 = cimg::cut(y0,0,h1), cy1 = cimg::cut(y1,0,h1) + step;
+      const int step = y0<=y1?1:-1, cy0 = cimg::cut(y0,0,h1), cy1 = cimg::cut(y1,0,h1) + step;
+      const longT
+        hdy01 = dy01*cimg::sign(dx01)/2,
+        hdy01tx = dy01*cimg::sign(dtx01)/2,
+        hdy01ty = dy01*cimg::sign(dty01)/2;
+
       dy01+=dy01?0:1;
 
       for (int y = cy0; y!=cy1; y+=step) {
-        const int
-          yy0 = y - y0,
+        const longT
+          yy0 = (longT)y - y0,
           x = x0 + (dx01*yy0 + hdy01)/dy01,
           tx = tx0 + (dtx01*yy0 + hdy01tx)/dy01,
           ty = ty0 + (dty01*yy0 + hdy01ty)/dy01;
@@ -45785,9 +45786,8 @@ namespace cimg_library_suffixed {
       if (std::min(y0,y1)>=height() || std::max(y0,y1)<0 || std::min(x0,x1)>=width() || std::max(x0,x1)<0) return *this;
 
       float iz0 = 1/z0, iz1 = 1/z1;
-      int
-        w1 = width() - 1, h1 = height() - 1,
-        dx01 = x1 - x0, dy01 = y1 - y0;
+      int w1 = width() - 1, h1 = height() - 1;
+      longT dx01 = (longT)x1 - x0, dy01 = (longT)y1 - y0;
       float
         diz01 = iz1 - iz0,
         txz0 = tx0*iz0, txz1 = tx1*iz1,
@@ -45806,14 +45806,14 @@ namespace cimg_library_suffixed {
       if (init_hatch) hatch = ~0U - (~0U>>1);
       cimg_init_scanline(opacity);
 
-      const int
-        step = y0<=y1?1:-1, hdy01 = dy01*cimg::sign(dx01)/2,
-        cy0 = cimg::cut(y0,0,h1), cy1 = cimg::cut(y1,0,h1) + step;
+      const int step = y0<=y1?1:-1, cy0 = cimg::cut(y0,0,h1), cy1 = cimg::cut(y1,0,h1) + step;
+      const longT hdy01 = dy01*cimg::sign(dx01)/2;
+
       dy01+=dy01?0:1;
 
       for (int y = cy0; y!=cy1; y+=step) {
-        const int
-          yy0 = y - y0,
+        const longT
+          yy0 = (longT)y - y0,
           x = x0 + (dx01*yy0 + hdy01)/dy01;
         const float
           iz = iz0 + diz01*yy0/dy01,
@@ -45880,9 +45880,8 @@ namespace cimg_library_suffixed {
       if (std::min(y0,y1)>=height() || std::max(y0,y1)<0 || std::min(x0,x1)>=width() || std::max(x0,x1)<0) return *this;
 
       float iz0 = 1/z0, iz1 = 1/z1;
-      int
-        w1 = width() - 1, h1 = height() - 1,
-        dx01 = x1 - x0, dy01 = y1 - y0;
+      int w1 = width() - 1, h1 = height() - 1;
+      longT dx01 = (longT)x1 - x0, dy01 = (longT)y1 - y0;
       float
         diz01 = iz1 - iz0,
         txz0 = tx0*iz0, txz1 = tx1*iz1,
@@ -45901,14 +45900,14 @@ namespace cimg_library_suffixed {
       if (init_hatch) hatch = ~0U - (~0U>>1);
       cimg_init_scanline(opacity);
 
-      const int
-        step = y0<=y1?1:-1, hdy01 = dy01*cimg::sign(dx01)/2,
-        cy0 = cimg::cut(y0,0,h1), cy1 = cimg::cut(y1,0,h1) + step;
+      const int step = y0<=y1?1:-1, cy0 = cimg::cut(y0,0,h1), cy1 = cimg::cut(y1,0,h1) + step;
+      const longT hdy01 = dy01*cimg::sign(dx01)/2;
+
       dy01+=dy01?0:1;
 
       for (int y = cy0; y!=cy1; y+=step) {
-        const int
-          yy0 = y - y0,
+        const longT
+          yy0 = (longT)y - y0,
           x = x0 + (dx01*yy0 + hdy01)/dy01;
         const float
           iz = iz0 + diz01*yy0/dy01,

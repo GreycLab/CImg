80e582a0c385ee8ba66485a27609eee0fefa7106
David Tschumperlé <david.tschumperle@gmail.com>
2022-06-28 16:41:14 +0200

    .

--- CImg.h
+++ CImg.h
@@ -54,7 +54,7 @@
 
 // Set version number of the library.
 #ifndef cimg_version
-#define cimg_version 314
+#define cimg_version 315
 
 /*-----------------------------------------------------------
  #

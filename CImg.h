3486b3ee73f13ba23138b4022960670854a9a525
David Tschumperle <David.Tschumperle@unicaen.fr>
2022-12-16 19:43:29 +0100

    Remove unused version of _sprintf().

--- CImg.h
+++ CImg.h
@@ -2403,16 +2403,6 @@ namespace cimg_library {
       return result;
     }
 
-    inline int _sprintf(char *const s, const char *const format, ...) {
-      cimg::mutex(6);
-      va_list args;
-      va_start(args,format);
-      const int result = std::vsnprintf(s,(size_t)~0U,format,args);
-      va_end(args);
-      cimg::mutex(6,0);
-      return result;
-    }
-
     inline int _snprintf(char *const s, const size_t n, const char *const format, ...) {
       cimg::mutex(6);
       va_list args;

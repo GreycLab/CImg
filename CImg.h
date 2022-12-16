07040bb0e16d7cf970914b1e69534534b89288b7
David Tschumperle <David.Tschumperle@unicaen.fr>
2022-12-16 18:31:42 +0100

    Fix warning about using 'vsprintf()' with clang-12.

--- CImg.h
+++ CImg.h
@@ -2407,7 +2407,7 @@ namespace cimg_library {
       cimg::mutex(6);
       va_list args;
       va_start(args,format);
-      const int result = std::vsprintf(s,format,args);
+      const int result = std::vsnprintf(s,(size_t)~0U,format,args);
       va_end(args);
       cimg::mutex(6,0);
       return result;

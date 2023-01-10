a9633d4eddf5cceb1a12edf1225ec68954477c16
David Tschumperle <David.Tschumperle@unicaen.fr>
2023-01-10 18:31:38 +0100

    .

--- CImg.h
+++ CImg.h
@@ -26742,19 +26742,20 @@ namespace cimg_library {
 
       static double mp_med(_cimg_math_parser& mp) {
         const unsigned int i_end = (unsigned int)mp.opcode[2];
-        if (i_end==5) { // Only one argument
+        CImg<double> values;
+        if (i_end==5) { // Only a single argument
           if ((unsigned)mp.opcode[4]==1) return _mp_arg(3); // Real value
-          else return CImg<double>(&_mp_arg(3),(unsigned int)mp.opcode[4],1,1,true).median(); // Vector value
-        }
-        unsigned int siz = 0;
-        for (unsigned int i = 4; i<i_end; i+=2) siz+=(unsigned int)mp.opcode[i];
-        CImg<double> values(siz);
-        double *ptr = values;
-        for (unsigned int i = 3; i<i_end; i+=2) {
-          const unsigned int len = (unsigned int)mp.opcode[i + 1];
-          if (len>1) std::memcpy(ptr,&_mp_arg(i),len*sizeof(double));
-          else *ptr = _mp_arg(i);
-          ptr+=len;
+          else values.assign(&_mp_arg(3),(unsigned int)mp.opcode[4],1,1,1,true); // Vector value
+        } else {
+          unsigned int siz = 0;
+          for (unsigned int i = 4; i<i_end; i+=2) siz+=(unsigned int)mp.opcode[i];
+          double *ptr = values;
+          for (unsigned int i = 3; i<i_end; i+=2) {
+            const unsigned int len = (unsigned int)mp.opcode[i + 1];
+            if (len>1) std::memcpy(ptr,&_mp_arg(i),len*sizeof(double));
+            else *ptr = _mp_arg(i);
+            ptr+=len;
+          }
         }
         return values.median();
       }

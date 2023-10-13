f595c7ceb9098495b8bf15b2b824652d741f3b01
David Tschumperle <David.Tschumperle@unicaen.fr>
2023-10-13 17:35:32 +0200

    .

--- CImg.h
+++ CImg.h
@@ -54,7 +54,7 @@
 
 // Set version number of the library.
 #ifndef cimg_version
-#define cimg_version 331
+#define cimg_version 332
 
 /*-----------------------------------------------------------
  #

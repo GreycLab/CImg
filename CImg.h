8e1754a3340b4b792d5362e0d2ef079d7f2ad76e
David Tschumperle <David.Tschumperle@unicaen.fr>
2025-04-03 11:00:41 +0200

    .

--- CImg.h
+++ CImg.h
@@ -12379,6 +12379,7 @@ namespace cimg_library {
 
     template<typename T>
     static void screenshot(const int x0, const int y0, const int x1, const int y1, CImg<T>& img) {
+      cimg::unused(x0,y0,x1,y1,img);
     }
 
 #endif

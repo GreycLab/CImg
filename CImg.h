1c5114e8757ec2de9fa2416773f0a0c7eee8e912
David Tschumperle <David.Tschumperle@unicaen.fr>
2025-11-22 10:39:56 +0100

    Start work on 3.6.5.

--- CImg.h
+++ CImg.h
@@ -54,7 +54,7 @@
 
 // Set version number of the library.
 #ifndef cimg_version
-#define cimg_version 364
+#define cimg_version 365
 
 /*-----------------------------------------------------------
  #

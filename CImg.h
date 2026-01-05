dd8679894453941f82ac7b1ee6001c79481da591
David Tschumperle <David.Tschumperle@unicaen.fr>
2026-01-05 11:56:05 +0100

    Prepare work for next version (3.6.7).

--- CImg.h
+++ CImg.h
@@ -54,7 +54,7 @@
 
 // Set version number of the library.
 #ifndef cimg_version
-#define cimg_version 366
+#define cimg_version 367
 
 /*-----------------------------------------------------------
  #

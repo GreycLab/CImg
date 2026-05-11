7437c9991a6f9ccdce741b5ec3ac61c81cf185b3
David Tschumperlé <david.tschumperle@unicaen.fr>
2026-05-11 11:46:50 +0200

    .

--- CImg.h
+++ CImg.h
@@ -18714,8 +18714,19 @@ namespace cimg_library {
                   else // From scalar
                     CImg<ulongT>::vector((ulongT)mp_vector_init,arg1,(ulongT)size(arg1),1,arg3,0).
                       move_to(code);
-                } else // Scalar
-                  CImg<ulongT>::vector((ulongT)mp_copy,arg1,arg3).move_to(code);
+                } else { // Scalar
+                  CImg<ulongT> &pop = code.back();
+                  if ((pop.size()==3 || pop.size()==4) && pop[1]==arg3 && pop[2]==arg1) {
+                    // Spot cases 'x = f(x)' and 'x = f(x,y)' -> in-place modification of x
+                    pop[1] = arg1;
+//                    if (mempos==arg3 + 1) --mempos;
+                  } else if (pop.size()==4 && pop[1]==arg3 && pop[3]==arg1) {
+                    // Spot case 'y = f(x,y)' -> in-place modification of y
+                    pop[1] = arg1;
+//                    if (mempos==arg3 + 1) --mempos;
+                  } else
+                    CImg<ulongT>::vector((ulongT)mp_copy,arg1,arg3).move_to(code);
+                }
               }
               return_comp = false;
               _cimg_mp_return(arg1);

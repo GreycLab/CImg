c8eff36fe2c642914a62a87d11a033570b01363a
David Tschumperlé <david.tschumperle@unicaen.fr>
2026-06-13 21:38:11 +0200

    Prepare work for next release '3.8.0'.

--- CImg.h
+++ CImg.h
@@ -54,7 +54,7 @@
 
 // Set version number of the library.
 #ifndef cimg_version
-#define cimg_version 377
+#define cimg_version 380
 
 /*-----------------------------------------------------------
  #
